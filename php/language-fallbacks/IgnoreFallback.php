d3ac40f8e16557fe3959d3a0c55df2c81aeccae3
MikeSvacha <47323762+MikeSvacha@users.noreply.github.com>
2019-05-07 09:41:36 +0200

    Update IgnoreFallback.php

--- php/language-fallbacks/IgnoreFallback.php
+++ php/language-fallbacks/IgnoreFallback.php
@@ -5,12 +5,10 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use KenticoCloud\Delivery\DeliveryClient;
-use KenticoCloud\Delivery\QueryParams;
 
 $client = new DeliveryClient('975bf280-fd91-488c-994c-2f04416e5ee3');
 
 $items = $client->getItems((new QueryParams())
-            ->language('en-US')
-            ->equals('system.language', 'en-US')
-            ->equals('system.type', 'article'));
+            ->language('es-ES')
+            ->equals('system.language', 'es-ES');
 // EndDocSection
