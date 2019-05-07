c09dfe220dee1cb4de0bbf3291ea763d613a24fb
MikeSvacha <47323762+MikeSvacha@users.noreply.github.com>
2019-05-07 09:36:54 +0200

    Update IgnoreFallback.swift

--- ios/language-fallbacks/IgnoreFallback.swift
+++ ios/language-fallbacks/IgnoreFallback.swift
@@ -3,9 +3,8 @@ import KenticoCloud
  
 let client = DeliveryClient.init(projectId:"975bf280-fd91-488c-994c-2f04416e5ee3")
  
-let customQuery = "items?language=en-US&system.language=en-US&system.type=article"
+let customQuery = "items?language=es-ES&system.language=es-ES"
 
-// More about strongly-typed models https://github.com/Kentico/cloud-sdk-swift#using-strongly-typed-models
 client.getItems(modelType: Article.self, customQuery: customQuery) { (isSuccess, itemsResponse, error) in
       if isSuccess {
            if let articles = itemsResponse?.items {
