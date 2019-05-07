343177007c21bb0c3c48ee05e4d52ce99bd28cd3
MikeSvacha <47323762+MikeSvacha@users.noreply.github.com>
2019-05-07 13:45:32 +0200

    Update IgnoreFallback.rb

--- ruby/language-fallbacks/IgnoreFallback.rb
+++ ruby/language-fallbacks/IgnoreFallback.rb
@@ -2,9 +2,7 @@
 require 'delivery-sdk-ruby'
 
 delivery_client = KenticoCloud::Delivery::DeliveryClient.new project_id: '975bf280-fd91-488c-994c-2f04416e5ee3'
-delivery_client.items([
-                        'system.language'.eq('es-ES')
-                      ])
+delivery_client.items(['system.language'.eq('es-ES')])
                .language('es-ES')
                .execute do |response|
                  items = response.items
