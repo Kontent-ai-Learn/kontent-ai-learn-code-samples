460fec4e3407ea3d3b87c04ad9951c2cdc5cbe81
MikeSvacha <47323762+MikeSvacha@users.noreply.github.com>
2019-05-07 14:02:22 +0200

    Update IgnoreFallback.js

--- js/language-fallbacks/IgnoreFallback.js
+++ js/language-fallbacks/IgnoreFallback.js
@@ -1,5 +1,5 @@
 // DocSection: language_fallbacks_ignore
-// const KenticoCloud = require('kentico-cloud-delivery');
+const KenticoCloud = require('kentico-cloud-delivery');
 
 const deliveryClient = new KenticoCloud.DeliveryClient({
     projectId: '975bf280-fd91-488c-994c-2f04416e5ee3',
