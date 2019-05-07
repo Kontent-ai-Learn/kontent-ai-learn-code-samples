1e3dded4267103c7a04b2e43a1789cdfb383e721
MikeSvacha <47323762+MikeSvacha@users.noreply.github.com>
2019-05-07 09:37:34 +0200

    Update IgnoreFallback.java

--- java/language-fallbacks/IgnoreFallback.java
+++ java/language-fallbacks/IgnoreFallback.java
@@ -4,11 +4,9 @@ import com.kenticocloud.delivery;
 DeliveryClient client = new DeliveryClient("975bf280-fd91-488c-994c-2f04416e5ee3");
 
 List<NameValuePair> params = DeliveryParameterBuilder.params()
-    .language("en-US")
-    .filterEquals("system.language", "en-US")
-    .filterEquals("system.type", "article")
+    .language("es-ES")
+    .filterEquals("system.language", "es-ES")
     .build();
 
-// Create strongly typed models according to https://developer.kenticocloud.com/docs/strongly-typed-models
-List<ArticleItem> items = client.getItems(ArticleItem.class, params);
+ContentItemsListingResponse listingResponse = client.getItems();
 // EndDocSection
