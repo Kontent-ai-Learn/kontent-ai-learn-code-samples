2c049d15faffaf1111fea68dae5fe1a1a3a1551e
Martina Farkasova <MartinaF@kentico.com>
2019-08-12 15:58:18 +0200

    Add code samples for Delete item with CM API v2

---
+++ js/cm-api-v2/DeleteItem.js
@@ -0,0 +1,21 @@
+// DocSection: cm_api_v2_delete_item
+// Using ES6 syntax
+import { ContentManagementClient } from 'kentico-cloud-content-management';
+
+const client = new ContentManagementClient({
+  projectId: '<YOUR_PROJECT_ID>',
+  apiKey: '<YOUR_API_KEY>'
+});
+
+client.deleteContentItem()
+  .byItemId('f4b3fc05-e988-4dae-9ac1-a94aba566474')
+  // .byItemCodename('on_roasts')
+  // .byItemExternalId('59714')
+  .toObservable()
+  .subscribe((response) => {
+    console.log(response);
+  },
+    (error) => {
+      console.log(error);
+    });
+// EndDocSection
---
+++ net/cm-api-v2/DeleteItem.cs
@@ -0,0 +1,17 @@
+// DocSection: cm_api_v2_delete_item
+using KenticoCloud.ContentManagement;
+
+ContentManagementOptions options = new ContentManagementOptions
+{
+    ApiKey = "<YOUR_API_KEY>",
+    ProjectId = "<YOUR_PROJECT_ID>"
+};
+
+ContentManagementClient client = new ContentManagementClient(options);
+
+ContentItemIdentifier identifier = ContentItemIdentifier.ById(Guid.Parse("f4b3fc05-e988-4dae-9ac1-a94aba566474"));
+// ContentItemIdentifier identifier = ContentItemIdentifier.ByCodename("on_roasts");
+// ContentItemIdentifier identifier = ContentItemIdentifier.ByExternalId("59713");
+
+await client.DeleteContentItemAsync(ContentItemIdentifier identifier);
+// EndDocSection
