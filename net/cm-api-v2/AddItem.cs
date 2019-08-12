8096037d2a0f5cf046adb8f0edcc9268bdb147d4
Martina Farkasova <MartinaF@kentico.com>
2019-08-12 14:34:02 +0200

    Add code samples for Add item with CM API v2

---
+++ net/cm-api-v2/AddItem.cs
@@ -0,0 +1,20 @@
+// DocSection: cm_api_v2_add_items
+using KenticoCloud.ContentManagement;
+
+ContentManagementOptions options = new ContentManagementOptions
+{
+    ApiKey = "<YOUR_API_KEY>",
+    ProjectId = "<YOUR_PROJECT_ID>"
+};
+
+ContentManagementClient client = new ContentManagementClient(options);
+
+ContentItemCreateModel item = new ContentItemCreateModel
+{
+    Name = "On Roasts",
+    Type = ContentTypeIdentifier.ByCodename("article"),
+    ExternalId = "59713";
+};
+
+ContentItemModel responseItem = await client.CreateContentItemAsync(item);
+// EndDocSection
