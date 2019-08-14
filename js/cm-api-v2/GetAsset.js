1fd5904c1c1f01c0f1cf3e38f8a38b33fe39449a
Martina Farkasova <MartinaF@kentico.com>
2019-08-14 16:00:37 +0200

    Add code samples for Get asset with CM API v2

---
+++ js/cm-api-v2/GetAsset.js
@@ -0,0 +1,20 @@
+// DocSection: cm_api_v2_get_asset
+// Using ES6 syntax
+import { ContentManagementClient } from 'kentico-cloud-content-management';
+
+const client = new ContentManagementClient({
+  projectId: '<YOUR_PROJECT_ID>',
+  apiKey: '<YOUR_API_KEY>'
+});
+
+client.viewAsset()
+  .byAssetId('0270ea18-4842-4d09-9570-17b41bb37e2d')
+  // .byAssetExternalId('which-brewing-fits-you')
+  .toObservable()
+  .subscribe((response) => {
+    console.log(response);
+  },
+    (error) => {
+      console.log(error);
+    });
+// EndDocSection
