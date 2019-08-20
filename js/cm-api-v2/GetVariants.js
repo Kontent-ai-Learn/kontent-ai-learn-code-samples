da17d1c3c281d3a3404d505c6d8962a2e7f9e4dd
Jan Cerman <janc@kentico.com>
2019-08-20 15:24:39 +0200

    Add code samples for the language variant endpoints in CM API v2

---
+++ js/cm-api-v2/GetVariants.js
@@ -0,0 +1,21 @@
+// DocSection: cm_api_v2_get_variants
+// Using ES6 syntax
+import { ContentManagementClient } from 'kentico-cloud-content-management';
+
+const client = new ContentManagementClient({
+  projectId: '<YOUR_PROJECT_ID>',
+  apiKey: '<YOUR_API_KEY>'
+});
+
+client.listLanguageVariants()
+  .byItemId('f4b3fc05-e988-4dae-9ac1-a94aba566474')
+  // .byItemCodename('on_roasts')
+  // .byItemExternalId('59713')
+  .toObservable()
+  .subscribe((response) => {
+    console.log(response);
+  },
+    (error) => {
+      console.log(error);
+    });
+// EndDocSection
