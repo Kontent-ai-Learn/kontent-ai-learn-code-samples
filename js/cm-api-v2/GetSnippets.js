e7dd1ac0513204c93af3dc8484262795ab312f51
Martina Farkasova <MartinaF@kentico.com>
2019-08-21 13:45:45 +0200

    Add code samples for List snippets with CM API v2

---
+++ js/cm-api-v2/GetSnippets.js
@@ -0,0 +1,18 @@
+// DocSection: cm_api_v2_get_snippets
+// Using ES6 syntax
+import { ContentManagementClient } from 'kentico-cloud-content-management';
+
+const client = new ContentManagementClient({
+  projectId: '<YOUR_PROJECT_ID>',
+  apiKey: '<YOUR_API_KEY>'
+});
+
+client.listContentTypeSnippets()
+  .toObservable()
+  .subscribe((response) => {
+    console.log(response);
+  },
+    (error) => {
+      console.log(error);
+    });
+// EndDocSection
