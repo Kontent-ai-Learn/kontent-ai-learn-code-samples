927a6910d7dd40692ef222b84d79858658cf60a4
Martina Farkasova <MartinaF@kentico.com>
2019-08-22 08:58:37 +0200

    Add code samples for Delete snippet with CM API v2

---
+++ js/cm-api-v2/DeleteSnippet.js
@@ -0,0 +1,21 @@
+// DocSection: cm_api_v2_delete_snippet
+// Using ES6 syntax
+import { ContentManagementClient } from 'kentico-cloud-content-management';
+
+const client = new ContentManagementClient({
+  projectId: '<YOUR_PROJECT_ID>',
+  apiKey: '<YOUR_API_KEY>'
+});
+
+client.deleteContentTypeSnippet()
+  .byTypeId('baf884be-531f-441f-ae88-64205efdd0f6')
+  // .byTypeCodename('metadata')
+  // .byTypeExternalId('snippet-type-123')
+  .toObservable()
+  .subscribe((response) => {
+    console.log(response);
+  },
+    (error) => {
+      console.log(error);
+    });
+// EndDocSection
