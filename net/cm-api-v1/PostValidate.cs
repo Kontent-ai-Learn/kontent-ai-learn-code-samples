3e9fd1808627fee0bd44ce395bad6d4f8e681953
Tomas Nosek <TomasN@kentico.com>
2019-09-19 16:20:54 +0200

    CTC-614 Add code samples for Project validation in CM API v1

---
+++ net/cm-api-v1/PostValidate.cs
@@ -0,0 +1,15 @@
+// DocSection: cm_api_v1_post_validate
+using KenticoCloud.ContentManagement;
+using KenticoCloud.ContentManagement.Models.ProjectReport;
+
+ContentManagementOptions options = new ContentManagementOptions
+{
+    ApiKey = "<YOUR_API_KEY>",
+    ProjectId = "<YOUR_PROJECT_ID>"
+};
+
+ContentManagementClient client = new ContentManagementClient(options);
+
+// Checks your project and stores the validation report in the response
+ProjectReportModel response = await client.ValidateProjectAsync();
+// EndDocSection
