a0704413df5c89cd37ece91885390241645d1e62
Aaron Collier <aaronc@kentico.com>
2019-09-24 13:43:28 +0200

    Update for strongly typed models

--- net/strongly-typed-models/GenerateModels.sh
+++ net/strongly-typed-models/GenerateModels.sh
@@ -1,3 +1,3 @@
 # DocSection: strongly_typed_models_generators
-CloudModelGenerator.exe --projectid 8d20758c-d74c-4f59-ae04-ee928c0816b7 --outputdir "C:\Users\Kentico\HelloHeadlessWorld\GeneratedModels" --withtypeprovider --structuredmodel
+KontentModelGenerator.exe --projectid 8d20758c-d74c-4f59-ae04-ee928c0816b7 --withtypeprovider --structuredmodel
 # EndDocSection
