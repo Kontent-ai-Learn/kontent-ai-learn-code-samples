f42178aae8f3fc0e4f3fa8b874e390f228db53c1
Aaron Collier <aaronc@kentico.com>
2019-09-24 15:12:16 +0200

    Add examples for post item

---
+++ net/cm-api-v1/PostItem.cs
@@ -0,0 +1,21 @@
+// DocSection: cm_api_v1_post_item
+using KenticoCloud.ContentManagement;
+
+ContentManagementOptions options = new ContentManagementOptions
+{
+    ApiKey = "<YOUR_API_KEY>",
+    ProjectId = "<YOUR_PROJECT_ID>"
+};
+
+ContentManagementClient client = new ContentManagementClient(options);
+
+ContentItemCreateModel item = new ContentItemCreateModel
+{
+    Name = "On Roasts",
+    Type = ContentTypeIdentifier.ByCodename("article"),
+    SitemapLocations = new[] { SitemapNodeIdentifier.ByCodename("articles") },
+    ExternalId = "59713";
+};
+
+ContentItemModel responseItem = await client.CreateContentItemAsync(item);
+// EndDocSection
