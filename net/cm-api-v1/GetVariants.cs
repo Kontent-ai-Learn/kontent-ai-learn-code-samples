75c05ad969d34a7dad4951c28ef5e0cbbd4c46b4
Martina Farkasova <MartinaF@kentico.com>
2019-09-25 15:44:16 +0200

    Add code samples for Language variant endpoints in CM API v1

---
+++ net/cm-api-v1/GetVariants.cs
@@ -0,0 +1,17 @@
+// DocSection: cm_api_v1_get_variants
+using KenticoCloud.ContentManagement;
+
+ContentManagementOptions options = new ContentManagementOptions
+{
+    ApiKey = "<YOUR_API_KEY>",
+    ProjectId = "<YOUR_PROJECT_ID>"
+};
+
+ContentManagementClient client = new ContentManagementClient(options);
+
+ContentItemIdentifier identifier = ContentItemIdentifier.ById((Guid.Parse("f4b3fc05-e988-4dae-9ac1-a94aba566474")));
+// ContentItemIdentifier identifier = ContentItemIdentifier.ByCodename("on_roasts");
+// ContentItemIdentifier identifier = ContentItemIdentifier.ByExternalId("59713");
+
+IEnumerable<ContentItemVariantModel<MyContentTypeModel>> responseVariants = await client.ListContentItemVariantsAsync<MyContentTypeModel>(identifier);
+// EndDocSection
