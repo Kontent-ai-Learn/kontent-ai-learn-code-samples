14f64e73bd76547a7e8ca016a4b8315c8da524a8
Aaron Collier <aaronc@kentico.com>
2019-09-25 10:09:50 +0200

    Add examples for content items CM v1

---
+++ net/cm-api-v1/PutItem.cs
@@ -0,0 +1,22 @@
+// DocSection: cm_api_v1_put_item
+using KenticoCloud.ContentManagement;
+
+ContentManagementOptions options = new ContentManagementOptions
+{
+    ApiKey = "<YOUR_API_KEY>",
+    ProjectId = "<YOUR_PROJECT_ID>"
+};
+
+ContentManagementClient client = new ContentManagementClient(options);
+
+string externalId = "59713";
+
+ContentItemUpsertModel item = new ContentItemUpsertModel()
+{
+    Name = "On Roasts",
+    Type = ContentTypeIdentifier.ByCodename("article"),
+  	SitemapLocations = new[] { SitemapNodeIdentifier.ByCodename("articles") }
+};
+
+ContentItemModel contentItemResponse = await client.UpsertContentItemByExternalIdAsync(externalId, item);
+// EndDocSection
