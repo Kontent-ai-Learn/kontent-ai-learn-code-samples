b987a28759b40dea0f5fb9fdeb063ee1b1fae8db
Aaron Collier <AaronC@kentico.com>
2019-09-27 14:05:05 +0200

    Update recommendation SDK namespace

--- net/recommendation-api/GetRecommendedItems.cs
+++ net/recommendation-api/GetRecommendedItems.cs
@@ -1,5 +1,5 @@
 // DocSection: recommendation_api_get_recommended_items
-using KenticoCloud.Recommender;
+using Kentico.Kontent.Recommender;
 
 RecommendationClient recommendationClient = new RecommendationClient("<YOUR_API_KEY>", timeout_seconds);
 RecommendedContentItem[] recommendedArticles = await recommendationClient.GetRecommendationsAsync("visitor01","item_codename_0", 3, "blog");
--- net/recommendation-api/PostRecommendedItems.cs
+++ net/recommendation-api/PostRecommendedItems.cs
@@ -1,5 +1,5 @@
 // DocSection: recommendation_api_post_recommended_items
-using KenticoCloud.Recommender;
+using Kentico.Kontent.Recommender;
 
 RecommendationClient recommendationClient = new RecommendationClient("<YOUR_API_KEY>", timeout_seconds);
 RecommendedContentItem[] recommendedArticles = await recommendationClient.GetRecommendationsAsync("item_codename_0", Request, Response, 3, "blog", sourceApp: "web");
