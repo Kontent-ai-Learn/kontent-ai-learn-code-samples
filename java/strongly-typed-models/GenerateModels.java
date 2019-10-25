1572573a12c74740ccbe3eb7454a563888299a14
Michael Svacha <MichaelS@kentico.com>
2019-10-25 10:13:09 +0200

    returned the model generator to prerenaming

--- java/strongly-typed-models/GenerateModels.java
+++ java/strongly-typed-models/GenerateModels.java
@@ -11,7 +11,7 @@ kenticoModel {
 }
 
 dependencies {
-    compile('com.github.kentico:kontent-delivery:1.0.3')
+    compile('com.kenticocloud:delivery-sdk-java:1.0.3')
 }
 
 // Runs generateModels task
