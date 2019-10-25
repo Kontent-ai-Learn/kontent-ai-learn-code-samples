0f0c702706099bce00a728f272877ff86a6437e6
Michael Svacha <MichaelS@kentico.com>
2019-10-25 10:00:18 +0200

    put model generator back to 1.0.3

--- java/strongly-typed-models/GenerateModels.java
+++ java/strongly-typed-models/GenerateModels.java
@@ -11,7 +11,7 @@ kenticoModel {
 }
 
 dependencies {
-    compile('com.github.kentico:kontent-delivery:3.0.0')
+    compile('com.github.kentico:kontent-delivery:1.0.3')
 }
 
 // Runs generateModels task
