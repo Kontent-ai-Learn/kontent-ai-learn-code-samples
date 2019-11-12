ab540a1c04b3d8d8c7c18f12ce1a12f32f44fd41
Aaron Collier <AaronC@kentico.com>
2019-11-12 15:14:50 +0100

    CTC-708 -- Examples for taxonomy group codenames

--- js/cm-api-v2/PostTaxonomyGroup.js
+++ js/cm-api-v2/PostTaxonomyGroup.js
@@ -10,38 +10,43 @@ const client = new ManagementClient({
 client.addTaxonomy()
   .withData(
     {
-      "name": "Personas",
-      "externalId": "Tax-Group-123",
-      "terms": [
+      name: "Personas",
+      externalId: "Tax-Group-123",
+      codename: "people",
+      terms: [
         {
-          "name": "Coffee expert",
-          "externalId": "Tax-term-456",
-          "terms": [
+          name: "Coffee expert",
+          externalId: "Tax-term-456",
+          codename: "expert",
+          terms: [
             {
-              "name": "Barista",
-              "externalId": "Tax-term-789",
-              "terms": []
+              name: "Barista",
+              externalId: "Tax-term-789",
+              terms: []
             },
             {
-              "name": "Cafe owner",
-              "externalId": "Tax-term-101",
-              "terms": []
+              name: "Cafe owner",
+              externalId: "Tax-term-101",
+              terms: []
             }
           ]
         },
         {
-          "name": "Coffee enthusiast",
-          "externalId": "Tax-term-112",
-          "terms": [
+          name: "Coffee enthusiast",
+          codename: "enthusiast",
+          externalId: "Tax-term-112",
+          terms: [
             {
-              "name": "Coffee lover",
-              "externalId": "Tax-term-131",
-              "terms": []
+              name: "Coffee lover",
+              codename: "lover",
+              externalId: "Tax-term-131",
+              terms: []
             },
             {
-              "name": "Coffee blogger",
-              "externalId": "Tax-term-145",
-              "terms": []
+              name: "Coffee blogger",
+              codename: "blogger",
+              externalId: "Tax-term-145",
+              terms: []
             }
           ]
         }
