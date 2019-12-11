1ccf8ed629b2fb5c0f169a6078e88c6097073101
Jan Cerman <janc@kentico.com>
2019-12-11 11:15:53 +0100

    Update SDK namespace for JavaRx

--- android/getting-latest-articles/GetArticles.java
+++ android/getting-latest-articles/GetArticles.java
@@ -1,7 +1,7 @@
 // DocSection: latest_articles_get_articles
 // Tip: Find more about Java/JavaRx SDKs at https://docs.kontent.ai/javaandroid
-import com.kenticocloud.delivery_core.*;
-import com.kenticocloud.delivery_rx.*;
+import com.github.kentico.kontent_delivery_core.*;
+import com.github.kentico.kontent_delivery_rx..*;
 
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
