2a94161d8703505997155206a9f0f636a0845779
Jan Cerman <janc@kentico.com>
2019-12-12 15:30:09 +0100

    Update GetTypedContent.java
    Rebrand code comment

--- java/strongly-typed-models/GetTypedContent.java
+++ java/strongly-typed-models/GetTypedContent.java
@@ -7,7 +7,7 @@ import java.lang.System;
 // Creates an instance of the DeliveryClient 
 DeliveryClient client = new DeliveryClient("8d20758c-d74c-4f59-ae04-ee928c0816b7");
 
-// Gets a content item from Kentico Cloud by its codename and maps it to its strongly-typed model
+// Gets a content item from Kentico Kontent by its codename and maps it to its strongly typed model
 try {
   Homepage homepage = client.getItem("hello_headless_world", Homepage.class);
   // Use homepage
