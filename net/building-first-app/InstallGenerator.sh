79f758b996bce7998777a2cc9de3bf9e84c3e7f3
Jan Cerman <janc@kentico.com>
2020-04-27 16:32:21 +0200

    CTC-892 Fix ID for install generator

--- net/building-first-app/InstallGenerator.sh
+++ net/building-first-app/InstallGenerator.sh
@@ -1,4 +1,4 @@
-# DocSection: building_first_net_app_install_generator
+# DocSection: building_first_net_app_init_generator
 # Installs the Kontent model generator tool locally into the app's folder
 dotnet tool restore
 # EndDocSection
