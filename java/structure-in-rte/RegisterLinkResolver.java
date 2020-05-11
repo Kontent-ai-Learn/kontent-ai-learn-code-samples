e41f318c0c68b12f4c25fd1fabf06ff30091d681
Martina Farkasova <MartinaF@kentico.com>
2020-05-11 11:12:23 +0200

    CTC-940 Add redirect rules for GitHub links

--- java/structure-in-rte/RegisterLinkResolver.java
+++ java/structure-in-rte/RegisterLinkResolver.java
@@ -1,5 +1,5 @@
 // DocSection: structure_in_rte_register_link_resolver
-// You can also register lambdas with the DeliveryClient as the resolvers are functional interfaces: https://github.com/Kentico/kontent-delivery-sdk-java/wiki/Resolving-links-to-content-items#registering-a-resolver
+// You can also register lambdas with the DeliveryClient as the resolvers are functional interfaces: https://docs.kontent.ai/java-register-resolver
 
 // Sets the resolver as an optional dependency of the DeliveryClient
 DeliveryClient client = new DeliveryClient("<YOUR_PROJECT_ID>"); 
--- ts/structure-in-rte/ImplementLinkResolver.ts
+++ ts/structure-in-rte/ImplementLinkResolver.ts
@@ -1,5 +1,5 @@
 // DocSection: structure_in_rte_implement_link_resolver
-// This is a global resolver for all links to articles. You can also define a resolver for a given query: https://github.com/Kentico/kontent-delivery-sdk-js/blob/master/DOCS.md#resolving-url-slugs-on-query-level
+// This is a global resolver for all links to articles. You can also define a resolver for a given query: https://docs.kontent.ai/js-resolve-query
 
 import { ContentItem, Elements } from '@kentico/kontent-delivery';
 
