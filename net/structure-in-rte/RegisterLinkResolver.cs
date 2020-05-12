d78d4d329580cfd25f7dd8bd33e6036d050840ed
Martina Farkasova <MartinaF@kentico.com>
2020-05-12 13:38:41 +0200

    One more redirect rule for .NET added.

--- net/structure-in-rte/RegisterLinkResolver.cs
+++ net/structure-in-rte/RegisterLinkResolver.cs
@@ -1,5 +1,5 @@
 // DocSection: structure_in_rte_register_link_resolver
-// You can also register it in IServiceCollection or another framework for dependency injection: https://github.com/Kentico/kontent-delivery-sdk-net/wiki/Resolving-links-to-content-items#registering-a-resolver
+// You can also register it in IServiceCollection or another framework for dependency injection: https://docs.kontent.ai/net-register-resolver
 using Kentico.Kontent.Delivery;
 
 // Sets the resolver as an optional dependency of the `IDeliveryClient` instance
