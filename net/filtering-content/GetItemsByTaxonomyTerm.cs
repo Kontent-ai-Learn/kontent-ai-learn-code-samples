a4b97ac1bef96e6ad55d6d01bfb22207712658e4
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-06-17 14:45:34 +0200

    282_final

---
+++ net/filtering-content/GetItemsByTaxonomyTerm.cs
@@ -0,0 +1,16 @@
+// DocSection: filtering_get_items_by_taxonomy_term
+DeliveryItemListingResponse<object> response = await _deliveryClient.GetItemsAsync<object>(
+  // Get articles tagged with specific tag
+  new ContainsFilter("elements.tags", "kentico")
+);
+
+DeliveryItemListingResponse<object> response = await _deliveryClient.GetItemsAsync<object>(
+  ///Get articles tagged with any of the below tags
+  new AnyFilter("elements.tags", "sport", "soccer")
+);
+
+DeliveryItemListingResponse<object> response = await _deliveryClient.GetItemsAsync<object>(
+  //Get articles tagged with all of the below tags
+  new AllFilter("elements.tags", "mvc", "kontent", "headless")
+);
+// EndDocSection
