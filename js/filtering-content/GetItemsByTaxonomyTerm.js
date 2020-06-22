0b4e2615c73bbcdea89150e6db4e2cc7b23d3a16
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-06-22 12:27:26 +0200

    CTC-282

---
+++ js/filtering-content/GetItemsByTaxonomyTerm.js
@@ -0,0 +1,13 @@
+// DocSection: filtering_get_items_by_taxonomy_term
+deliveryClient.items()
+  // Get articles tagged with specific tag
+  .containsFilter("elements.tags", ["kentico"])
+
+deliveryClient.items()
+  ///Get articles tagged with any of the below tags
+  .anyFilter("elements.tags", ["football"], ["soccer"])
+
+deliveryClient.items()
+  //Get articles tagged with all of the below tags
+  .allFilter("elements.tags", ["mvc"], ["kontent"], ["headless"])
+// EndDocSection
