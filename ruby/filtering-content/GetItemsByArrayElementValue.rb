4f4d9ec1e5c0bd82ee4dd473556e0d9a49924ec2
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-06-23 14:36:59 +0200

    CTC-282

--- ruby/filtering-content/GetItemsByArrayElementValue.rb
+++ ruby/filtering-content/GetItemsByArrayElementValue.rb
@@ -1,11 +1,11 @@
 // DocSection: filtering_get_item_by_id
 delivery_client.items([
-  // Gets item by its ID
+  # Get items where multiple choice has a specific value
   'elements.multiple_choice'.contains('value')
 ])
 
 delivery_client.items([
-  // Gets item by its ID
+  # Get items where multiple choice has any of the below values
   'elements.multiple_choice'.contains('value1', 'value2')
 ])
 // EndDocSection
