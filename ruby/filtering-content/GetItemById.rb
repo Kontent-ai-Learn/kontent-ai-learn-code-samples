05d09e891aa69872483c74bdaeb481c79f8c363b
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-06-23 14:37:22 +0200

    CTC-282

--- ruby/filtering-content/GetItemById.rb
+++ ruby/filtering-content/GetItemById.rb
@@ -1,6 +1,6 @@
 // DocSection: filtering_get_item_by_id
 delivery_client.items([
-  // Gets item by its ID
+  # Gets item by its ID
   'system.id'.equals('2f7288a1-cfc8-47be-9bf1-b1d312f7da18')
 ])
 // EndDocSection
