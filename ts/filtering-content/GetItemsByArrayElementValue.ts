a7e21cee24af094037811ce8408b80da7690b165
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-06-23 09:13:29 +0200

    CTC-282

---
+++ ts/filtering-content/GetItemsByArrayElementValue.ts
@@ -0,0 +1,9 @@
+// DocSection: filtering_get_items_by_array_element_value
+deliveryClient.items<ContentItem>()
+  // Get items where multiple choice has a specific value
+  .containsFilter("elements.multiple_choice", ["value"])
+
+deliveryClient.items<ContentItem>()
+  // Get items where multiple choice has any of the below values
+  .anyFilter("elements.multiple_choice", ["value1"], ["value2"])
+// EndDocSection
