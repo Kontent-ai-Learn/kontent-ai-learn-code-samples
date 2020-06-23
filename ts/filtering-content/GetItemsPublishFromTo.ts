5c568ff2cfeecf9f128b643f3e97f5371a500ba1
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-06-23 09:44:02 +0200

    CTC-282

---
+++ ts/filtering-content/GetItemsPublishFromTo.ts
@@ -0,0 +1,17 @@
+// DocSection: filtering_get_published_from_to
+deliveryClient.items<ContentItem>()
+  // Gets items whose publish date is at least 2020-05-10, 00:00:00
+  .greaterThanFilter("elements.publish_date", "2020-05-10")
+
+deliveryClient.items<ContentItem>()
+  // Gets items whose publish date is at least 2020-05-10, 00:00:00
+  .greaterThanOrEqualFilter("elements.publish_date", "2020-05-10")
+
+deliveryClient.items<ContentItem>()
+  // Gets items whose publish date is at most 2020-05-19, 23:59:00
+  .lessThanFilter("elements.publish_date", "2020-05-20")
+
+deliveryClient.items<ContentItem>()
+  // Gets items whose publish date is at most 2020-05-19, 23:59:00
+  .lessThanOrEqualFilter("elements.publish_date", "2020-05-20")
+// EndDocSection
