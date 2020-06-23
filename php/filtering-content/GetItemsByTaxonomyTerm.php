ac8f82d3b3288564dcd8cc6f5ef65701f1af7c96
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-06-23 11:34:49 +0200

    CTC-282

---
+++ php/filtering-content/GetItemsByTaxonomyTerm.php
@@ -0,0 +1,13 @@
+// DocSection: filtering_get_items_by_taxonomy_term
+$items = $client->getItems((new QueryParams())
+  // Get articles tagged with specific tag
+  ->contains("elements.tags", ["kentico"])
+
+$items = $client->getItems((new QueryParams())
+  ///Get articles tagged with any of the below tags
+  ->any("elements.tags", ["sport"], ["soccer"])
+
+$items = $client->getItems((new QueryParams())
+  //Get articles tagged with all of the below tags
+  ->all("elements.tags", ["mvc"], ["kontent"], ["headless"])
+// EndDocSection
