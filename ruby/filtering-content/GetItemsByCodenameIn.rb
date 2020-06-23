8f29888c996eb2730c1cfedd64317d350143c282
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-06-23 14:37:43 +0200

    CTC-282

--- ruby/filtering-content/GetItemsByCodenameIn.rb
+++ ruby/filtering-content/GetItemsByCodenameIn.rb
@@ -1,6 +1,6 @@
 // DocSection: filtering_get_item_by_id
 delivery_client.items([
-  // Gets items whose codename is one of the following
+  # Gets items whose codename is one of the following
   'system.codename'.in('welcome_to_mvc', 'top_10_websites', 'kontent_best_practices')
 ])
 // EndDocSection
