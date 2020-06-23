06ad8f1f27f4145986a896d54d1ae3db77442f9d
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-06-23 13:42:07 +0200

    CTC-282

--- android/filtering-content/GetItemsByTaxonomyTerm.java
+++ android/filtering-content/GetItemsByTaxonomyTerm.java
@@ -1,13 +1,13 @@
 // DocSection: filtering_get_items_by_taxonomy_term
-List<NameValuePair> params = DeliveryParameterBuilder.params()
+List<object> items = deliveryService.<object>items()
   // Get articles tagged with specific tag
   .filterContains("elements.tags", Arrays.asList("kentico"))
 
-List<NameValuePair> params = DeliveryParameterBuilder.params()
+List<object> items = deliveryService.<object>items()
   ///Get articles tagged with any of the below tags
   .filterAny("elements.tags", Arrays.asList("sport", "soccer"))
 
-List<NameValuePair> params = DeliveryParameterBuilder.params()
+List<object> items = deliveryService.<object>items()
   //Get articles tagged with all of the below tags
   .filterAll("elements.tags", Arrays.asList("mvc", "kontent", "headless"))
 // EndDocSection
