46bb5c3a879b55173ee1871de274fb13ab1b6c97
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-06-23 09:16:31 +0200

    CTC-282

---
+++ ts/filtering-content/GetItemsByCodenameIn.ts
@@ -0,0 +1,5 @@
+// DocSection: filtering_get_items_by_codename_in
+deliveryClient.items<ContentItem>()
+  // Gets items whose codename is one of the following
+  .inFilter("system.codename", "welcome_to_mvc", "top_10_websites", "kontent_best_practices")
+// EndDocSection
