75b79077a21c1de6d0be37e371924fd53797ae40
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-06-24 09:59:43 +0200

    CTC-282

---
+++ ruby/filtering-content/GetItemsByUrlSlug.rb
@@ -0,0 +1,5 @@
+// DocSection: filtering_get_items_by_url_slug
+delivery_client.items([
+  // Gets items whose URL Slug is as follows - this is same as filtering by text element value
+  'elements.url_slug'.eq('article-title')
+// EndDocSection
