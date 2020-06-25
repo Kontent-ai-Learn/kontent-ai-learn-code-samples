82a1bc3f482aebe4767aff5889b1e9e328ebd46a
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-06-25 10:01:43 +0200

    CTC-282

---
+++ ios/filtering-content/GetItemsByArrayElementValue.swift
@@ -0,0 +1,31 @@
+// DocSection: filtering_get_item_by_id
+// Get items where multiple choice has a specific value
+let customQuery = "items?elements.multiple_choice[contains]=value"
+
+client.getItems(modelType: Article.self, customQuery: customQuery) { (isSuccess, itemsResponse, error) in
+    if isSuccess {
+        if let articles = itemsResponse?.items {
+            // Use your items here
+        }
+    } else {
+        if let error = error {
+            print(error)
+        }
+    }
+}
+
+// Get items where multiple choice has any of the below values
+let customQuery = "items?elements.multiple_choice[any]=value1,value2"
+
+client.getItems(modelType: Article.self, customQuery: customQuery) { (isSuccess, itemsResponse, error) in
+    if isSuccess {
+        if let articles = itemsResponse?.items {
+            // Use your items here
+        }
+    } else {
+        if let error = error {
+            print(error)
+        }
+    }
+}
+// EndDocSection
