98986ea01a9f92049f916169fe88167890798234
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-06-25 10:17:52 +0200

    CTC-282

--- ios/filtering-content/GetItemsByStringElementValue.swift
+++ ios/filtering-content/GetItemsByStringElementValue.swift
@@ -1,7 +1,7 @@
 // DocSection: filtering_get_items_by_string_element_value
+// Gets items whose Article title element equals "Get started with MVC"
 let customQuery = "items?elements.article_title=Get started with MVC"
 
-// More about strongly-typed models https://github.com/Kentico/kontent-delivery-sdk-swift#using-strongly-typed-models
 client.getItems(modelType: Article.self, customQuery: customQuery) { (isSuccess, itemsResponse, error) in
     if isSuccess {
         if let articles = itemsResponse?.items {
