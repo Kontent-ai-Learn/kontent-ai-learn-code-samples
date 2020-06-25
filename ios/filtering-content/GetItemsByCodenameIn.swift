f57b067690937ccb0b66506b0fa661c9d955b88f
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-06-25 10:06:26 +0200

    CTC-282

---
+++ ios/filtering-content/GetItemsByCodenameIn.swift
@@ -0,0 +1,15 @@
+// DocSection: filtering_get_item_by_id
+// Gets items whose codename is one of the following
+let customQuery = "items?system.codename[in]=welcome_to_mvc,top_10_websites,kontent_best_practices"
+
+client.getItems(modelType: Article.self, customQuery: customQuery) { (isSuccess, itemsResponse, error) in
+    if isSuccess {
+        if let articles = itemsResponse?.items {
+            // Use your items here
+        }
+    } else {
+        if let error = error {
+            print(error)
+        }
+    }
+}
