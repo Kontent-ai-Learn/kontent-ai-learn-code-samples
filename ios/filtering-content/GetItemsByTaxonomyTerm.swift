5602e27dffa4537928835027e1d820ded2f03fcf
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-06-25 10:16:09 +0200

    CTC-282

---
+++ ios/filtering-content/GetItemsByTaxonomyTerm.swift
@@ -0,0 +1,46 @@
+// DocSection: filtering_get_items_by_string_element_value
+// Get articles tagged with specific tag
+let customQuery = "items?elements.tags[contains]=kentico"
+
+client.getItems(modelType: Article.self, customQuery: customQuery) { (isSuccess, itemsResponse, error) in
+    if isSuccess {
+        if let articles = itemsResponse?.items {
+            // Use your items here
+        }
+    } else {
+        if let error = error {
+            print(error)
+        }
+    }
+}
+
+//Get articles tagged with any of the below tags
+let customQuery = "items?elements.tags[any]=sport,soccer"
+
+client.getItems(modelType: Article.self, customQuery: customQuery) { (isSuccess, itemsResponse, error) in
+    if isSuccess {
+        if let articles = itemsResponse?.items {
+            // Use your items here
+        }
+    } else {
+        if let error = error {
+            print(error)
+        }
+    }
+}
+
+//Get articles tagged with all of the below tags
+let customQuery = "items?elements.tags[all]=mvc,kontent,headless"
+
+client.getItems(modelType: Article.self, customQuery: customQuery) { (isSuccess, itemsResponse, error) in
+    if isSuccess {
+        if let articles = itemsResponse?.items {
+            // Use your items here
+        }
+    } else {
+        if let error = error {
+            print(error)
+        }
+    }
+}
+// EndDocSection
