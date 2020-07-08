775f20eb857bcea1036160f25902e9d9edc0a843
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-07-08 12:14:34 +0200

    fixed docsection to typeS CTC-282

--- android/filtering-content/GetItemsOfTypes.java
+++ android/filtering-content/GetItemsOfTypes.java
@@ -1,4 +1,4 @@
-// DocSection: filtering_get_items_of_type
+// DocSection: filtering_get_items_of_types
 List<NameValuePair> params = DeliveryParameterBuilder.params()
   // Gets items of content type Product
   .inFilter("system.type", Arrays.asList("article", "product", "news"))
