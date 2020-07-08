0a51a5ee58a21503aa9874f37feb8f47b76d4efa
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-07-08 12:36:54 +0200

    fixed docSection CTC-282

--- android/filtering-content/GetItemsByStringElementValue.java
+++ android/filtering-content/GetItemsByStringElementValue.java
@@ -1,4 +1,4 @@
-// DocSection: filtering_get_items_its_linked_item
+// DocSection: filtering_get_items_by_string_element_value
 List<object> items = deliveryService.<object>items()
   // Gets items whose Article title element equals "Get started with MVC"
   .filterEquals("elements.article_title", "Get started with MVC")
