6662bb68c215a1418145d075701acc2cf9c35069
Jan Cerman <janc@kentico.com>
2020-07-21 14:08:36 +0200

    CTC-282 Remove unsupported Swift example, JS/TS unification

--- js/filtering-content/GetItemsOfType.js
+++ js/filtering-content/GetItemsOfType.js
@@ -1,5 +1,6 @@
 // DocSection: filtering_get_items_of_type
 deliveryClient.items()
-  // Gets items of content type Product
-  .equalsFilter("system.type", "product")
+  // Gets items based on the type Product
+  .equalsFilter('system.type', 'product')
+  // Does the same as using .type('product')
 // EndDocSection
--- ts/filtering-content/GetItemsOfType.ts
+++ ts/filtering-content/GetItemsOfType.ts
@@ -1,5 +1,6 @@
 // DocSection: filtering_get_items_of_type
 deliveryClient.items<ContentItem>()
-  // Gets items of content type Product
+  // Gets items based on the type Product
   .equalsFilter("system.type", "product")
+  // Does the same as using .type('product')
 // EndDocSection
