d38b476f2d3c91d346522820304cbab71a2fcfc5
Jan Cerman <janc@kentico.com>
2020-07-23 12:29:00 +0200

    CTC-282 Change IDs for two scenarios

--- java/filtering-content/GetItemsPublishBetween.java
+++ java/filtering-content/GetItemsPublishBetween.java
@@ -1,4 +1,4 @@
-// DocSection: filtering_get_published_between
+// DocSection: filtering_get_items_by_range
 List<NameValuePair> params = DeliveryParameterBuilder.params()
   // Gets items whose publish date is at least 2020-05-10, 00:00:00 but no greater than 2020-05-20, 00:00:00
   .filterRange("elements.publish_date", "2020-05-10", "2020-05-20")
