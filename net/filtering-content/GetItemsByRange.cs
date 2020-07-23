5513d8c0ae61b8932cd88ab0e006173e07d1b298
Jan Cerman <janc@kentico.com>
2020-07-23 13:55:09 +0200

    CTC-282 Adjust filtering examples for datetime and range

--- net/filtering-content/GetItemsByRange.cs
+++ net/filtering-content/GetItemsByRange.cs
@@ -1,6 +1,6 @@
 // DocSection: filtering_get_items_by_range
-// system.last_modified value is stored with ms precision such as 2020-07-20T09:18:17.4857463Z
-// Date&Time element value is stored with minute precision such as 2020-04-29T00:00:00Z
+// system.last_modified value is stored with ms precision
+// Date&Time element value is stored with minute precision
 
 // Gets items modified between April 5, 2020 10:30 UTC and April 7, 2020, 7:00 UTC
 DeliveryItemListingResponse<object> response = await deliveryClient.GetItemsAsync<object>(
--- php/filtering-content/GetItemsByRange.php
+++ php/filtering-content/GetItemsByRange.php
@@ -3,7 +3,7 @@
 // system.last_modified value is stored with ms precision
 // Date&Time element value is stored with minute precision
 $items = $client->getItems((new QueryParams())
-  // Gets items whose publish date is at least 2020-05-10, 00:00:00 but no greater than 2020-05-20, 00:00:00
-  ->range("elements.publish_date", "2020-05-10", "2020-05-20")
+    // Gets items modified between April 5, 2020 10:30 UTC and April 7, 2020, 7:00 UTC
+    ->range("system.last_modified", "2020-05-05T10:30:00", "2020-05-07T07:00:00")
 // EndDocSection
 ?>
