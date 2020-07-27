bc6b76f9712c3030c53116c9420f058f2261a1ff
Jan Cerman <janc@kentico.com>
2020-07-27 09:26:59 +0200

    CTC-282 Remove duplicate array filtering scenario

--- android/filtering-content/GetItemsByArrayElementValue.java
+++
@@ -1,9 +0,0 @@
-// DocSection: filtering_get_items_by_array_element_value
-List<object> items = deliveryService.<object>items()
-  // Get items where multiple choice has a specific value
-  .containsFilter("elements.multiple_choice", Arrays.asList("value"))
-
-List<object> items = deliveryService.<object>items()
-  // Get items where multiple choice has any of the below values
-  .anyFilter("elements.multiple_choice", Arrays.asList("value1", "value2"))
-// EndDocSection
