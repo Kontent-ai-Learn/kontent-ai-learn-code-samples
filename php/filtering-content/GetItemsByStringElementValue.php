4836382125af7455c11a0dc964eefa1c6a76f73a
Jan Cerman <janc@kentico.com>
2020-07-27 09:14:24 +0200

    CTC-282 Fix incorrect docsection ID after sync

--- php/filtering-content/GetItemsByStringElementValue.php
+++ php/filtering-content/GetItemsByStringElementValue.php
@@ -1,5 +1,5 @@
 <?php
-// DocSection: 
+// DocSection: filtering_get_items_by_string_element_value
 $items = $client->getItems((new QueryParams())
     // Gets items whose Title element value equals to "Hello World"
     ->equals("elements.title", "Hello World")
