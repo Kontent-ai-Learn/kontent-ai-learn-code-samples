a48be5dff4ba7d1a49b417d1368bc696e52d86b1
Jan Cerman <janc@kentico.com>
2020-07-27 09:05:36 +0200

    CTC-282 Fix typo

--- js/filtering-content/GetItemsOfType.js
+++ js/filtering-content/GetItemsOfType.js
@@ -1,6 +1,6 @@
 // DocSection: filtering_get_items_of_type
 deliveryClient.items<ContentItem>()
-  // Gets items based on the type Product;
+  // Gets items based on the type Product
   .equalsFilter('system.type', 'product')
   // Same as using .type('product')
   .toObservable()
