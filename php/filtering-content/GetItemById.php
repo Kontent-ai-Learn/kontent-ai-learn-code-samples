e05608ecd11fcc4e9614a79001320c3163182319
Jan Cerman <janc@kentico.com>
2020-07-28 12:50:05 +0200

    CTC-282 Adjust filtering_get_item_by_id

--- php/filtering-content/GetItemById.php
+++ php/filtering-content/GetItemById.php
@@ -1,5 +1,7 @@
+<?
 // DocSection: filtering_get_item_by_id
+// Gets an item by its internal ID
 $items = $client->getItems((new QueryParams())
-  // Gets item by its ID
   ->equals('system.id', '2f7288a1-cfc8-47be-9bf1-b1d312f7da18')
 // EndDocSection
+?>
