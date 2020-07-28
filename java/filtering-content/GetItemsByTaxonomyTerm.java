5ce82fc258b201ff04de47c90a4d088878fc3aa6
Jan Cerman <janc@kentico.com>
2020-07-28 11:26:47 +0200

    CTC-282 Fix Java samples

--- java/filtering-content/GetItemsByTaxonomyTerm.java
+++ java/filtering-content/GetItemsByTaxonomyTerm.java
@@ -2,19 +2,19 @@
 // Note: Filters work with codenames of the tags.
 // Gest items tagged with a single tag
 List<NameValuePair> params1 = DeliveryParameterBuilder.params()
-    .filterContains("elements.tags", ["kontent"])
+    .filterContains("elements.tags", "kontent")
     .build();
 ContentItemsListingResponse listingResponse1 = deliveryClient.getItems(params1);
 
 // Gets items tagged with multiple tags
 List<NameValuePair> params2 = DeliveryParameterBuilder.params()
-    .filterAll("elements.tags", ["kontent", "headless"])
+    .filterAll("elements.tags", "kontent", "headless")
     .build();
 ContentItemsListingResponse listingResponse2 = deliveryClient.getItems(params2);
 
 // Gets items tagged with at least one of multiple tags
 List<NameValuePair> params3 = DeliveryParameterBuilder.params()
-    .filterAny("elements.tags", ["football"], ["soccer"])
+    .filterAny("elements.tags", "football", "soccer")
     .build();
 ContentItemsListingResponse listingResponse3 = deliveryClient.getItems(params3);
 // EndDocSection
--- java/filtering-content/GetItemsOfTypes.java
+++ java/filtering-content/GetItemsOfTypes.java
@@ -1,7 +1,7 @@
 // DocSection: filtering_get_items_of_types
 List<NameValuePair> params = DeliveryParameterBuilder.params()
     // Gets items based on the types Product, Article, and News
-    .filterIn("system.type", ["product"], ["article"], ["news"])
+    .filterIn("system.type", "product", "article", "news")
     .build();
 
 ContentItemsListingResponse listingResponse = deliveryClient.getItems(params);
