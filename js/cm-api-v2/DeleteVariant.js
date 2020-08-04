a943085688f9ff4134d3ae3d84686f0880534e0e
Keith Hammond <34716170+kahammond@users.noreply.github.com>
2020-08-04 04:56:31 -0400

    CTC-755 Missing JS SDK API Example (#18)
    * Create DeleteVariant.js
    
    CTC-755 Missing JS API Examples
    
    * Create GetTaxonomyGroup.js
    
    CTC-755 Updating files based on PR comments.
    
    * Create GetVariantsWithComponentsOfType.js
    
    CTC-755 Updating files based on PR comments.
    
    * Create PatchSnippet.js
    
    CTC-755 Updating files based on PR comments.
    
    * Update PatchSnippet.js
    
    Removed extra character from GUID

---
+++ js/cm-api-v2/DeleteVariant.js
@@ -0,0 +1,24 @@
+// DocSection: cm_api_v2_delete_variant
+// Tip: Find more about JS/TS SDKs at https://docs.kontent.ai/javascript
+// Using ES6 syntax
+import { ManagementClient } from '@kentico/kontent-management';
+
+const client = new ManagementClient({
+  projectId: '<YOUR_PROJECT_ID>',
+  apiKey: '<YOUR_API_KEY>'
+});
+
+client.deleteLanguageVariant()
+  .byItemId('f4b3fc05-e988-4dae-9ac1-a94aba566474')
+  // .byItemCodename('my_article')
+  // .byItemExternalId('59713')
+  .byLanguageId('d1f95fde-af02-b3b5-bd9e-f232311ccab8')
+  // .byLanguageCodename('es-ES')
+  .toObservable()
+  .subscribe((response) => {
+    console.log(response);
+  },
+    (error) => {
+      console.log(error);
+    });
+// EndDocSection
