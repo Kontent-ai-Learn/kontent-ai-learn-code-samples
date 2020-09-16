1b80672c60c6f1c5c383082b400774cc3e531b78
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-09-16 14:25:26 +0200

    Update CreateTaxonomy.js

--- js/importing-content-model/CreateTaxonomy.js
+++ js/importing-content-model/CreateTaxonomy.js
@@ -2,59 +2,59 @@
 // Tip: Find more about JS/TS SDKs at https://docs.kontent.ai/javascript
 
 var client = new KontentManagement.ManagementClient({
-			projectId: '<YOUR_PROJECT_ID>',
-			apiKey: '<YOUR_API_KEY>'
-		});
-    
+	projectId: '<YOUR_PROJECT_ID>',
+	apiKey: '<YOUR_API_KEY>'
+});
+
 client.addTaxonomy()
-  .withData(
-    {
-      name: 'Blogpost topic',
-      codename: 'blog_topic',
-      terms: [
-        {
-          name: 'Sport',
-          codename: 'sport',
-          terms: [
-            {
-              name: 'Soccer',
-              codename: 'soccer',
-              terms: []
-            },
-            {
-              name: 'Ice hockey',
-              codename: 'hockey',
-              terms: []
-            },
-            {
-              name: 'Rugby',
-              codename: 'rugby',
-              terms: []	  
-            }
-          ]
-        },
-        {
-          name: 'Technology stack',
-          codename: 'tech',
-          terms: [
-            {
-              name: 'JavaScript',
-              codename: 'js',
-              terms: []
-            },
-            {
-              name: 'C#',
-              codename: 'c',
-              terms: []
-            },
-            {
-             name: 'MVC', 
-             codename: 'mvc',
-             terms: []
-            }
-          ]
-        }
-      ]
-    }
-  )
-  // EndDocSection
+	.withData(
+		{
+			name: 'Blogpost topic',
+			codename: 'blog_topic',
+			terms: [
+				{
+					name: 'Sport',
+					codename: 'sport',
+					terms: [
+						{
+							name: 'Soccer',
+							codename: 'soccer',
+							terms: []
+						},
+						{
+							name: 'Ice hockey',
+							codename: 'hockey',
+							terms: []
+						},
+						{
+							name: 'Rugby',
+							codename: 'rugby',
+							terms: []	  
+						}
+					]
+				},
+				{
+					name: 'Technology stack',
+					codename: 'tech',
+					terms: [
+						{
+							name: 'JavaScript',
+							codename: 'js',
+							terms: []
+						},
+						{
+							name: 'C#',
+							codename: 'c',
+							terms: []
+						},
+						{
+							name: 'MVC', 
+							codename: 'mvc',
+							terms: []
+						}
+					]
+				}
+			]
+		}
+	)
+// EndDocSection
