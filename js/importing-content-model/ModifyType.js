5699154b80d9d75a29a7a8b17871861f903a8903
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-09-16 14:30:12 +0200

    Update ModifyType.js

--- js/importing-content-model/ModifyType.js
+++ js/importing-content-model/ModifyType.js
@@ -1,38 +1,36 @@
 // DocSection: importing_modify_type
 // Tip: Find more about JS/TS SDKs at https://docs.kontent.ai/javascript
-
 var client = new KontentManagement.ManagementClient({
-			projectId: '<YOUR_PROJECT_ID>',
-			apiKey: '<YOUR_API_KEY>'
-		});
-    
-    client.modifyContentType()
-  // .byTypeId("0be13600-e57c-577d-8108-c8d860330985")
-  .byTypeCodename("blogpost")
-  // .byTypeExternalId("my-article-id")
-.withData (
-    [
-	    {
-        op: "addInto",
-        path: "/content_groups",
-        value: {
-          name: 'Topic',
-		      external_id: 'topic'
-        }
-      },
-      {
-        op: "addInto",
-        path: "/elements",
-        value: {
-          taxonomy_group: {
-            codename: 'blog_topic'
-          },
-          type: 'taxonomy',
-          content_group: {
-            external_id: 'topic'
-          }
-        }
-      }
-    ]
-)
+    projectId: '<YOUR_PROJECT_ID>',
+    apiKey: '<YOUR_API_KEY>'
+});
+
+client.modifyContentType()
+    // .byTypeId("0be13600-e57c-577d-8108-c8d860330985")
+    .byTypeCodename("blogpost")
+    // .byTypeExternalId("my-article-id")
+    .withData(
+        [{
+                op: "addInto",
+                path: "/content_groups",
+                value: {
+                    name: 'Topic',
+                    external_id: 'topic'
+                }
+            },
+            {
+                op: "addInto",
+                path: "/elements",
+                value: {
+                    taxonomy_group: {
+                        codename: 'blog_topic'
+                    },
+                    type: 'taxonomy',
+                    content_group: {
+                        external_id: 'topic'
+                    }
+                }
+            }
+        ]
+    )
 // EndDocSection
