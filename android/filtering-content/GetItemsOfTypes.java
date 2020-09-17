eb202b0396a9070909be8ccc63b7c9f7875703fb
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-09-17 10:16:05 +0200

    Update GetItemsOfTypes.java

--- android/filtering-content/GetItemsOfTypes.java
+++ android/filtering-content/GetItemsOfTypes.java
@@ -4,4 +4,4 @@ List<ContentItem> items = deliveryService.<ContentItem>items()
     .inFilter("system.type", Arrays.asList("article", "product", "news"))
     .get()
     .getItems();
-// EndDocSection
+// EndDocSectio
