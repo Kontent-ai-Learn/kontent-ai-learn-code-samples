6ceccf72af1affb3a7d8d451986ab789d51cca67
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-10-05 13:06:08 +0200

    CTC-1027 Add samples for import content model (#23)
    * Create filtering-delivery-api
    
    * Delete filtering-delivery-api
    
    * Create GetItemsPublishFromTo
    
    * Update GetItemsPublishFromTo
    
    * Rename GetItemsPublishFromTo to GetItemsPublishFromTo.cs
    
    * Rename net/filtering-delivery-api/GetItemsPublishFromTo.cs to net/filtering-content/GetItemsPublishFromTo.cs
    
    * Update GetItemsPublishFromTo.cs
    
    * Update GetItemsPublishFromTo.cs
    
    * 282_initial commit
    
    * 282_final commit
    
    * 282_final
    
    * 282_final
    
    * 282_final
    
    * 282_final
    
    * 282_final
    
    * 282_final
    
    * 282_final
    
    * 282_final
    
    * 282_final
    
    * 282_final
    
    * 282_final
    
    * 282_final
    
    * 282_final
    
    * 282_final
    
    * Update GetItemsPublishFromTo.cs
    
    * 282_final
    
    * CTC-282
    
    * CT-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282
    
    * CTC-282 Fix review issues
    
    * fixed docsection to typeS CTC-282
    
    * fixed docSection CTC-282
    
    * fixed docSection and filter name CTC-282
    
    * fixed docSection CTC-282
    
    * fixed docSection CTC-282
    
    * CTC-1027
    
    * Update CreateSnippet.js
    
    * CTC-1027
    
    * CTC-1027
    
    * CTC-1027
    
    * Update CreateTaxonomy.js
    
    * Update CreateSnippet.js
    
    * CTC-1027
    
    * CTC-1027
    
    * CTC-1027
    
    * CTC-1027
    
    * Update CreateType.js
    
    * Update CreateTaxonomy.js
    
    * Update CreateSnippet.js
    
    * Update CreateTaxonomy.js
    
    * Update CreateType.js
    
    * Update ModifyType.js
    
    * Update CreateTaxonomy.curl
    
    * Update GetItemsByStringElementValue.java
    
    * Update GetItemsOfTypes.java
    
    * Delete GetItemsByArrayElementValue.rb
    
    * Update GetItemsByCodenameIn.rb
    
    * Update GetItemsByItsLinkedItem.rb
    
    * Update GetItemsByStringElementValue.java
    
    * Update GetItemsByStringElementValue.java
    
    * Update GetItemsOfTypes.java
    
    * Update GetItemsOfTypes.java
    
    * Update GetItemsOfTypes.java
    
    * fixed formatting
    
    * add docsection
    
    * fix formatting
    
    * fix formatting
    
    * add client
    
    * fix formatting
    
    * fix formatting
    
    * fix formatting
    
    * fix formatting
    
    * fix formatting
    
    * fix formatting
    
    Co-authored-by: Jan Cerman <janc@kentico.com>

---
+++ js/importing-content-model/CreateSnippet.js
@@ -0,0 +1,27 @@
+// DocSection: importing_create_snippet
+// Tip: Find more about JS/TS SDKs at https://docs.kontent.ai/javascript
+var client = new KontentManagement.ManagementClient({
+  projectId: '<YOUR_PROJECT_ID>',
+  apiKey: '<YOUR_API_KEY>'
+});
+
+client.addContentTypeSnippet()
+  .withData(builder => {
+    return {
+      name: 'Author',
+      codename: 'author',
+      elements: [
+        builder.textElement({
+          name: 'Name',
+          codename: 'name',
+          type: 'text'
+        }),
+        builder.assetElement({
+          name: 'Profile picture',
+          codename: 'profile_picture',
+          type: 'asset'
+        })
+      ]
+  };
+})
+// EndDocSection
