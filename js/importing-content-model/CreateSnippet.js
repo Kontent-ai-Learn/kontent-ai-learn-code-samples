2629993369b61bd50a74f62ace36f986895a2f34
Matúš Ušiak <43740719+matus666@users.noreply.github.com>
2020-10-05 10:26:19 +0200

    add docsection

--- js/importing-content-model/CreateSnippet.js
+++ js/importing-content-model/CreateSnippet.js
@@ -1,3 +1,5 @@
+// DocSection: importing_create_snippet
+// Tip: Find more about JS/TS SDKs at https://docs.kontent.ai/javascript
 var client = new KontentManagement.ManagementClient({
   projectId: '<YOUR_PROJECT_ID>',
   apiKey: '<YOUR_API_KEY>'
@@ -22,3 +24,4 @@ client.addContentTypeSnippet()
       ]
   };
 })
+// EndDocSection
