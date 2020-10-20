584451b8f9ed86897e393dad7661dc7c549e1836
ondrejch <ondrejch@kentico.com>
2020-10-20 19:42:00 +0200

    update android/getting-localized-content to new SDK syntax

--- android/getting-localized-content/GetLocalizedItemsWithSlug.java
+++ android/getting-localized-content/GetLocalizedItemsWithSlug.java
@@ -1,62 +1,53 @@
 // DocSection: getting_localized_content_url_slug
 // Tip: Find more about JavaRx SDK at https://docs.kontent.ai/androidandroid
-import com.github.kentico.kontent_delivery_core.*;
-import com.github.kentico.kontent_delivery_rx.*;
 
-import io.reactivex.Observer;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Function;
+// Prepares the DeliveryOptions configuration object
+DeliveryOptions options = DeliveryOptions.builder()
+    .projectId("<YOUR_PROJECT_ID>")
+    .build();
 
-// Prepares an array to hold strongly-typed models
-List<TypeResolver<?>> typeResolvers = new ArrayList<>();
+// Initializes a DeliveryClient for Java projects
+DeliveryClient client = new DeliveryClient(options);
 
-// Registers the type resolver for articles
-typeResolvers.add(new TypeResolver<>(Article.TYPE, new Function<Void, Article>() {
-    @Override
-    public Article apply(Void input) {
-        return new Article();
-    }
-}));
+// Registers the model class for articles
+client.registerType(Article.class);
 
-// Prepares the DeliveryService configuration object
-String projectId = "8d20758c-d74c-4f59-ae04-ee928c0816b7";
-IDeliveryConfig config = DeliveryConfig.newConfig(projectId)
-    .withTypeResolvers(typeResolvers);
-
-// Initializes a DeliveryService for Java projects
-IDeliveryService deliveryService = new DeliveryService(config);
+// Initializes a DeliveryClient for Java projects
+DeliveryClient client = new DeliveryClient(options);
 
 // Gets the Spanish variant of an "About us" content item that has "acerda-de-nosotros" in its "URL pattern" element using a simple request
-List<Article> article = deliveryService.<Article>items()
-    .languageParameter("es-ES")
-    .equalsFilter("system.type", "article")
-    .equalsFilter("elements.url_pattern", "acerda-de-nosotros")
-    .get()
-    .getItems();
+CompletionStage<List<Article>> articles = client.getItems(
+    Article.class,
+    DeliveryParameterBuilder.params()
+        .language("es-ES")
+        .filterEquals("elements.url_pattern", "acerca-de-nosotros")
+        .build()
+);
 
-// Gets the Spanish variant of an "About us" content item that has "acerda-de-nosotros" in its "URL pattern" element using RxJava2
-deliveryService.<Article>items()
-    .languageParameter("es-ES")
-    .equalsFilter("system.type", "article")
-    .equalsFilter("elements.url_pattern", "acerda-de-nosotros")
-    .getObservable()
-    .subscribe(new Observer<DeliveryItemListingResponse<Article>>() {
-        @Override
-        public void onSubscribe(Disposable d) {
-        }
+// Gets the Spanish variant of an "About us" content item that has "acerda-de-nosotros" in its "URL pattern" element using RxJava
+Observable.fromCompletionStage( client.getItems(
+    Article.class,
+    DeliveryParameterBuilder.params()
+        .language("es-ES")
+        .filterEquals("elements.url_pattern", "acerca-de-nosotros")
+        .build()
+)).subscribe(new Observer<List<Article>>() {
+    @Override
+    public void onSubscribe(@NonNull Disposable d) {
+    }
 
-        @Override
-        public void onNext(DeliveryItemResponse<Article> response) {
-            // Gets the content items
-            Article item = response.getItem();
-        }
+    @Override
+    public void onNext(@NonNull List<Article> articles) {
+        // Gets the content item
+        Article article = articles.get(0);
+    }
 
-        @Override
-        public void onError(Throwable e) {
-        }
+    @Override
+    public void onError(@NonNull Throwable e) {
+    }
 
-        @Override
-        public void onComplete() {
-        }
-    });
+    @Override
+    public void onComplete() {
+    }
+});
 // EndDocSection
