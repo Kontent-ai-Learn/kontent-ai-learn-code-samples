db54f2c823f295f9830bf72ca4dbecf4dd7084c5
ondrejch <ondrejch@kentico.com>
2020-10-20 20:19:49 +0200

    update android/securing-public-access to new SDK syntax

--- android/securing-public-access/GetArticle.java
+++ android/securing-public-access/GetArticle.java
@@ -1,39 +1,33 @@
 // DocSection: securing_public_access_get_article
 // Tip: Find more about JavaRx SDK at https://docs.kontent.ai/androidandroid
-import com.github.kentico.kontent_delivery_core.*;
-import com.github.kentico.kontent_delivery_rx.*;
 
-import io.reactivex.Observer;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Function;
+ // Prepares the DeliveryOptions configuration object
+ DeliveryOptions options = DeliveryOptions.builder()
+ .projectId("<YOUR_PROJECT_ID>")
+ .productionApiKey("<YOUR_API_KEY>")
+ .build();
 
-// Prepares the DeliveryService configuration object
-String projectId = "<YOUR_PROJECT_ID>";
-String secureApiKey = "<YOUR_API_KEY>";
-IDeliveryConfig config = DeliveryConfig.newConfig(projectId)
-    .withSecuredApiKey(secureApiKey);
+// Initializes a DeliveryClient for Java projects
+DeliveryClient client = new DeliveryClient(options);
 
 // Gets the latest version of a content item using a simple request
-ContentItem item = deliveryService.<ContentItem>item("my_article")
-    .get()
-    .getItem();
+CompletionStage<ContentItemResponse> article = client.getItem("my_article");
 
-// Gets the latest version of a content item using RxJava2
-deliveryService.<ContentItem>item("my_article")
-    .getObservable()
-    .subscribe(new Observer<DeliveryItemResponse<ContentItem>>() {
+// Gets the latest version of a content item using RxJava
+Observable.fromCompletionStage(client.getItem("my_article"))
+    .subscribe(new Observer<ContentItemResponse>() {
         @Override
-        public void onSubscribe(Disposable d) {
+        public void onSubscribe(@NonNull Disposable d) {
         }
 
         @Override
-        public void onNext(DeliveryItemResponse<ContentItem> response) {
+        public void onNext(@NonNull ContentItemResponse response) {
             // Gets the content item
             ContentItem item = response.getItem();
         }
 
         @Override
-        public void onError(Throwable e) {
+        public void onError(@NonNull Throwable e) {
         }
 
         @Override
