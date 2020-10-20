c4e41d6fad651b28abb3d05f4dadcc101e80f48d
ondrejch <ondrejch@kentico.com>
2020-10-20 19:58:24 +0200

    update android/managing-navigation to new SDK syntax

--- android/managing-navigation/GetArticlesDepth.java
+++ android/managing-navigation/GetArticlesDepth.java
@@ -1,58 +1,47 @@
 // DocSection: managing_navigation_articles_depth
 // Tip: Find more about JavaRx SDK at https://docs.kontent.ai/androidandroid
-import com.github.kentico.kontent_delivery_core.*;
-import com.github.kentico.kontent_delivery_rx.*;
 
-import io.reactivex.Observer;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Function;
+// Prepares the DeliveryOptions configuration object
+DeliveryOptions options = DeliveryOptions.builder()
+    .projectId("<YOUR_PROJECT_ID>")
+    .build();
 
-// Prepares an array to hold strongly-typed models
-List<TypeResolver<?>> typeResolvers = new ArrayList<>();
+// Initializes a DeliveryClient for Java projects
+DeliveryClient client = new DeliveryClient(options);
 
-// Registers the type resolver for articles
-typeResolvers.add(new TypeResolver<>(NavigationItem.TYPE, new Function<Void, NavigationItem>() {
-    @Override
-    public NavigationItem apply(Void input) {
-        return new NavigationItem();
-    }
-}));
-
-// Prepares the DeliveryService configuration object
-String projectId = "8d20758c-d74c-4f59-ae04-ee928c0816b7";
-IDeliveryConfig config = DeliveryConfig.newConfig(projectId)
-    .withTypeResolvers(typeResolvers);
-
-// Initializes a DeliveryService for Java projects
-IDeliveryService deliveryService = new DeliveryService(config);
+// Registers the model class for articles
+client.registerType(NavigationItem.class);
 
 // Gets navigation items and their linked items using a simple request
-NavigationItem root = deliveryService.<NavigationItem>item("root_navigation_item")
-    .depthParameter(5)
-    .get()
-    .getItem();
+CompletionStage<NavigationItem> root = client.getItem(
+    "root_navigation_item",
+     NavigationItem.class,
+     DeliveryParameterBuilder.params().linkedItemsDepth(5).build()
+);
 
-// Gets navigation items and their linked items using RxJava2
-deliveryService.<NavigationItem>item("root_navigation_item")
-    .depthParameter(5)
-    .getObservable()
-    .subscribe(new Observer<DeliveryItemResponse<NavigationItem>>() {
-        @Override
-        public void onSubscribe(Disposable d) {
-        }
 
-        @Override
-        public void onNext(DeliveryItemResponse<NavigationItem> response) {
-            // Gets the item
-            NavigationItem item = response.getItem();
-        }
+// Gets navigation items and their linked items using RxJava
+Observable.fromCompletionStage(client.getItem(
+    "root_navigation_item",
+    NavigationItem.class,
+    DeliveryParameterBuilder.params().linkedItemsDepth(5).build()
+)).subscribe(new Observer<NavigationItem>() {
+    @Override
+    public void onSubscribe(@NonNull Disposable d) {
+    }
 
-        @Override
-        public void onError(Throwable e) {
-        }
+    @Override
+    public void onNext(@NonNull NavigationItem item) {
+        // Gets the item
+        NavigationItem root = item;
+    }
 
-        @Override
-        public void onComplete() {
-        }
-    });
+    @Override
+    public void onError(@NonNull Throwable e) {
+    }
+
+    @Override
+    public void onComplete() {
+    }
+});
 // EndDocSection
--- android/managing-navigation/GetArticlesSlug.java
+++ android/managing-navigation/GetArticlesSlug.java
@@ -1,60 +1,44 @@
 // DocSection: managing_navigation_articles_slugs
 // Tip: Find more about JavaRx SDK at https://docs.kontent.ai/androidandroid
-import com.github.kentico.kontent_delivery_core.*;
-import com.github.kentico.kontent_delivery_rx.*;
 
-import io.reactivex.Observer;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Function;
+// Prepares the DeliveryOptions configuration object
+DeliveryOptions options = DeliveryOptions.builder()
+    .projectId("<YOUR_PROJECT_ID>")
+    .build();
 
-// Prepares an array to hold strongly-typed models
-List<TypeResolver<?>> typeResolvers = new ArrayList<>();
+// Initializes a DeliveryClient for Java projects
+DeliveryClient client = new DeliveryClient(options);
 
-// Registers the type resolver for articles
-typeResolvers.add(new TypeResolver<>(Article.TYPE, new Function<Void, Article>() {
-    @Override
-    public Article apply(Void input) {
-        return new Article();
-    }
-}));
-
-// Prepares the DeliveryService configuration object
-String projectId = "975bf280-fd91-488c-994c-2f04416e5ee3";
-IDeliveryConfig config = DeliveryConfig.newConfig(projectId)
-    .withTypeResolvers(typeResolvers);
-
-// Initializes a DeliveryService for Java projects
-IDeliveryService deliveryService = new DeliveryService(config);
+// Registers the model class for articles
+client.registerType(Article.class);
 
 // Gets specific elements of all articles using a simple request
-List<Article> articles = deliveryService.<Article>items()
-    .equalsFilter("system.type", "article")
-    .elementsParameter(Arrays.asList("title", "url_pattern"))
-    .get()
-    .getItems();
+CompletionStage<List<Article>> articles = client.getItems(
+    Article.class, 
+    DeliveryParameterBuilder.params().projection("title", "url_pattern").build()
+);
 
-// Gets specific elements of all articles using JavaRx2
-deliveryService.<Article>items()
-    .equalsFilter("system.type", "article")
-    .elementsParameter(Arrays.asList("title", "url_pattern"))
-    .getObservable()
-    .subscribe(new Observer<DeliveryItemListingResponse<Article>>() {
-        @Override
-        public void onSubscribe(Disposable d) {
-        }
+// Gets specific elements of all articles using JavaRx
+Observable.fromCompletionStage(client.getItems(
+    Article.class,
+    DeliveryParameterBuilder.params().projection("title", "url_pattern").build()
+)).subscribe(new Observer<List<Article>>() {
+    @Override
+    public void onSubscribe(@NonNull Disposable d) {
+    }
 
-        @Override
-        public void onNext(DeliveryItemResponse<ContentItem> response) {
-            // Gets the content items
-            Article item = response.getItem();
-        }
+    @Override
+    public void onNext(@NonNull List<Article> items) {
+        // Gets the content items
+        List<Article> articles = items;
+    }
 
-        @Override
-        public void onError(Throwable e) {
-        }
+    @Override
+    public void onError(@NonNull Throwable e) {
+    }
 
-        @Override
-        public void onComplete() {
-        }
-    });
+    @Override
+    public void onComplete() {
+    }
+});
 // EndDocSection
