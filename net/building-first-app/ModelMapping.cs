a47ccdd2e8b6f771a0534550efa8ad101ff44dc4
Jan Cerman <janc@kentico.com>
2020-12-11 13:22:00 +0100

    CTC-1189 Adjust examples for build .NET app

--- net/building-first-app/ModelMapping.cs
+++ net/building-first-app/ModelMapping.cs
@@ -1,20 +1,15 @@
 // DocSection: building_first_net_app_model_mapping
-// Tip: Find more about .NET SDKs at https://docs.kontent.ai/net
-using Kentico.Kontent.Delivery;
-
-// Creates an instance of the delivery client
-// ProTip: Use DI for this in your apps https://docs.kontent.ai/net-register-client
-IDeliveryClient client = DeliveryClientBuilder
-      .WithProjectId("<YOUR_PROJECT_ID>")
-      .Build();
-
 // Retrieves 3 articles ordered by their post date
-IDeliveryItemListingResponse<Article> response = await client.GetItemsAsync<Article>(
-    new EqualsFilter("system.type", "article"),
-    new LimitParameter(3),
-    new OrderParameter("elements.post_date", SortOrder.Descending)
-);
+public async Task<ViewResult> Index()
+{
+    var response = await DeliveryClient.GetItemsAsync<Article>(
+        new EqualsFilter("system.type", "article"),
+        new LimitParameter(3),
+        new DepthParameter(0),
+        new OrderParameter("elements.post_date")
+    );
 
-// Sends the strongly-typed content items to a View
-return View(response.Items)
+    // Sends the strongly-typed content items to a View 
+    return View(response.Items);
+}
 // EndDocSection
