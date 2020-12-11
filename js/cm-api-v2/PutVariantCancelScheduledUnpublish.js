a6132039d5a5c064029b13cfb8692e7520698690
edison23 <klementdavid@gmail.com>
2020-12-11 18:22:23 +0100

    CTC-1175: REST sample for cancel-scheduled-unpublish added. JS sample skeleton prepared for when the SDK gets updated.

---
+++ js/cm-api-v2/PutVariantCancelScheduledUnpublish.js
@@ -0,0 +1,12 @@
+// DocSection: cm_api_v2_put_var_cancel_sched_unpublish
+// Tip: Find more about JS/TS SDKs at https://docs.kontent.ai/javascript
+// Using ES6 syntax
+import { ManagementClient } from '@kentico/kontent-management';
+
+const client = new ManagementClient({
+  projectId: '<YOUR_PROJECT_ID>',
+  apiKey: '<YOUR_API_KEY>'
+});
+
+// The rest TBD when the SDK gets updated
+// EndDocSection
