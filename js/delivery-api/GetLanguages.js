43e65330b8774cff06b1c4b458d33774b5bf18dd
Jan Cerman <janc@kentico.com>
2021-01-20 15:26:18 +0100

    CTC-1199 Add samples for DAPI languages endpoint

---
+++ js/delivery-api/GetLanguages.js
@@ -0,0 +1,13 @@
+// DocSection: delivery_api_get_languages
+// Tip: Find more about JS/TS SDKs at https://docs.kontent.ai/javascript
+const KontentDelivery = require('@kentico/kontent-delivery');
+
+const deliveryClient = new KontentDelivery.DeliveryClient({
+  projectId: '<YOUR_PROJECT_ID>'
+});
+
+deliveryClient.languages()
+  .limitParameter(3)
+  .toObservable()
+  .subscribe(response => console.log(response.languages));
+// EndDocSection
---
+++ net/delivery-api/GetLanguages.cs
@@ -0,0 +1,18 @@
+// DocSection: delivery_api_get_languages
+// Tip: Find more about .NET SDKs at https://docs.kontent.ai/net
+using Kentico.Kontent.Delivery;
+
+// Creates an instance of the delivery client
+// ProTip: Use DI for this in your apps https://docs.kontent.ai/net-register-client
+IDeliveryClient client = DeliveryClientBuilder
+      .WithProjectId("<YOUR_PROJECT_ID>")
+      .Build();
+
+// Gets 3 languages
+IDeliveryLanguageListingResponse response = await deliveryClient.GetLanguagesAsync(
+    new List<IQueryParameter>() {
+        new LimitParameter(3)
+    });
+
+IList<ILanguage> languages = response.Languages;
+// EndDocSection
