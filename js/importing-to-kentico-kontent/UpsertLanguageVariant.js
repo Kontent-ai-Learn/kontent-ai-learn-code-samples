77e91b8c0babe2e25b9adfd9bcc13dd613700dbf
Jan Cerman <janc@kentico.com>
2021-02-09 07:56:45 +0100

    Fix upsert variant for JS MAPIv2 in tutorial

--- js/importing-to-kentico-kontent/UpsertLanguageVariant.js
+++ js/importing-to-kentico-kontent/UpsertLanguageVariant.js
@@ -11,49 +11,50 @@ const client = new ManagementClient({
 client.upsertLanguageVariant()
   .byItemExternalId('ext-cafe-brno')
   .byLanguageCodename('en-US')
-  .withElements([
-    {
+  .withData((builder) => [
+    builder.textElement({
+
       element: {
         codename: 'street'
       },
       value: 'Nove Sady 25'
-    },
-    {
+    }),
+    builder.textElement({
       element: {
         codename: 'city'
       },
       value: 'Brno'
-    },
-    {
+    }),
+    builder.textElement({
       element: {
         codename: 'country'
       },
       value: 'Czech Republic'
-    },
-    {
+    }),
+    builder.textElement({
       element: {
         codename: 'state'
       },
       value: 'Jihomoravsky kraj'
-    },
-    {
+    }),
+    builder.numberElement({
       element: {
         codename: 'zip_code'
       },
-      value: '60200'
-    },
-    {
+      value: 60200
+    }),
+    builder.numberElement({
       element: {
         codename: 'phone'
       },
-      value: '+420 555 555 555'
-    },
-    {
+      value: 420555555555
+    }),
+    builder.textElement({
       element: {
         codename: 'email'
       },
       value: 'brnocafe@kontent.ai'
-    }
+    })
   ])
   .toObservable()
   .subscribe((response) => {
