cf78488c01e08dfe02c87210d925c9c5ab88f918
Jan Cerman <janc@kentico.com>
2021-10-01 13:13:25 +0200

    Remove obsolete code for content personalization scenarios

--- net/recommendation-api/GetRecommendedItems.cs
+++
@@ -1,7 +0,0 @@
-// DocSection: recommendation_api_get_recommended_items
-// Tip: Find more about .NET SDKs at https://docs.kontent.ai/net
-using Kentico.Kontent.Recommender;
-
-RecommendationClient recommendationClient = new RecommendationClient("<YOUR_API_KEY>", timeout_seconds);
-RecommendedContentItem[] recommendedArticles = await recommendationClient.GetRecommendationsAsync("visitor01","item_codename_0", 3, "blog");
-// EndDocSection
--- net/recommendation-api/PostRecommendedItems.cs
+++
@@ -1,7 +0,0 @@
-// DocSection: recommendation_api_post_recommended_items
-// Tip: Find more about .NET SDKs at https://docs.kontent.ai/net
-using Kentico.Kontent.Recommender;
-
-RecommendationClient recommendationClient = new RecommendationClient("<YOUR_API_KEY>", timeout_seconds);
-RecommendedContentItem[] recommendedArticles = await recommendationClient.GetRecommendationsAsync("item_codename_0", Request, Response, 3, "blog", sourceApp: "web");
-// EndDocSection
