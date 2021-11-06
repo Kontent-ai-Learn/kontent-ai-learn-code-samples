687322ebf896f42c654653506c888d2d1d654741
Ondřej Chrastina <chrastina.ondra@gmail.com>
2021-11-06 19:43:00 +0100

    update importing-linked-content scenario samples

--- net/importing-linked-content/UpsertSecondVariant.cs
+++ net/importing-linked-content/UpsertSecondVariant.cs
@@ -1,25 +1,32 @@
 // DocSection: import_linked_upsert_Sec_variant
 // Tip: Find more about .NET SDKs at https://docs.kontent.ai/net
-// Using Management API v1
 using Kentico.Kontent.Management;
 
-ManagementOptions options = new ManagementOptions
+var client = new ManagementClient(new ManagementOptions
 {
     ApiKey = "<YOUR_API_KEY>",
     ProjectId = "<YOUR_PROJECT_ID>"
-};
+});
 
-ManagementClient client = new ManagementClient(options);
+var identifier = new LanguageVariantIdentifier(Reference.ByExternalId("456"), Reference.ByCodename("en-US"));
 
-ArticleModel stronglyTypedElements = new ArticleModel
+await client.UpsertLanguageVariantAsync(identifier, new LanguageVariantUpsertModel
 {
-    Title = "Donate with us",
-    RelatedArticles = new [] { ContentItemIdentifier.ByExternalId("123") }
-};
-
-ContentItemIdentifier itemIdentifier = ContentItemIdentifier.ByExternalId("456");
-LanguageIdentifier languageIdentifier = LanguageIdentifier.ByCodename("en-US");
-ContentItemVariantIdentifier identifier = new ContentItemVariantIdentifier(itemIdentifier, languageIdentifier);
-
-ContentItemVariantModel<ArticleModel> responseVariant = await client.UpsertContentItemVariantAsync<ArticleModel>(identifier, stronglyTypedElements);
+    Elements = new dynamic[]
+    {
+        new TextElement
+        {
+            Element = Reference.ByCodename("title"),
+            Value = "Donate with us"
+        }.ToDynamic(),
+        new LinkedItemsElement
+        {
+            Element = Reference.ByCodename("related_articles"),
+            Value = new []
+            {
+                Reference.ByExternalId("123"),
+            }
+        }.ToDynamic()
+    }
+});
 // EndDocSection
--- net/importing-linked-content/ValidateContent.cs
+++ net/importing-linked-content/ValidateContent.cs
@@ -1,16 +1,14 @@
 // DocSection: import_linked_validate_content
 // Tip: Find more about .NET SDKs at https://docs.kontent.ai/net
-// Using Management API v1
 using Kentico.Kontent.Management;
-using Kentico.Kontent.Management.Models.ProjectReport;
 
-ManagementOptions options = new ManagementOptions
+var client = new ManagementClient(new ManagementOptions
 {
     ApiKey = "<YOUR_API_KEY>",
     ProjectId = "<YOUR_PROJECT_ID>"
-};
+});
 
 ManagementClient client = new ManagementClient(options);
 
-ProjectReportModel response = await client.ValidateProjectAsync();
+var response = await client.ValidateProjectAsync();
 // EndDocSection
