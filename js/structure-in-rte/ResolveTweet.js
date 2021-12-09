f5a7bb7952e79fb2c3e74e7a698f0c5b91fa7a2b
Jan Cerman <janc@kentico.com>
2021-12-09 12:32:50 +0100

    CTC-1471 Fix typo in code comment

--- js/structure-in-rte/ResolveTweet.js
+++ js/structure-in-rte/ResolveTweet.js
@@ -13,7 +13,7 @@ const response = await deliveryClient.item('my_article')
 // Stores the contents of the rich text element.
 const richTextElement = response.data.item.elements.body;
 
-// Note: The code below works run correctly in browser. To adjust the code for nodejs, see https://docs.kontent.ai/js-rte-nodejs.
+// Note: The code below executes correctly in browser. To adjust the code for nodejs, see https://docs.kontent.ai/js-rte-nodejs.
 // Defines how to resolve the rich text element
 const resolvedRichText = KontentDelivery.createRichTextHtmlResolver().resolveRichText({
   // Gives the resolver the contents of your rich text.
