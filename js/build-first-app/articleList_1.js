da196a21aaa458f756f66d8056e8012f343ea1ce
Jan Cerman <janc@kentico.com>
2021-12-21 11:56:20 +0100

    CTC-1471 Shorten DocSection ID

--- js/build-first-app/articleList_1.js
+++ js/build-first-app/articleList_1.js
@@ -1,4 +1,4 @@
-// DocSection: build_first_js_app_articlelist_1_addtoelementbyid
+// DocSection: build_first_js_app_articlelist_1
 // Add list container to app
 const articleList = addToElementbyId('div', 'article-list', app);
 // EndDocSection
