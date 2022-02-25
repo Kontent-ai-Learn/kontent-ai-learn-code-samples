fd4986b28a0ca65b5bb8bbaa76cc41d86d466c65
Ondřej Chrastina <chrastina.ondra@gmail.com>
2022-02-25 14:27:01 +0100

    add put subscription methods

---
+++ net/management-api-v2/PutSubscriptionUserDeactivate.cs
@@ -0,0 +1,15 @@
+// DocSection: cm_api_v2_put_subscription_user_deactivate
+// Tip: Find more about .NET SDKs at https://kontent.ai/learn/net
+using Kentico.Kontent.Management;
+
+var client = new ManagementClient(new ManagementOptions
+{
+    ApiKey = "<YOUR_API_KEY>",
+    ProjectId = "<YOUR_PROJECT_ID>"
+});
+
+var identifier = UserIdentifier.ByEmail("user@kentico.com");
+//var identifier = UserIdentifier.ById("d94bc87a-c066-48a1-a910-4f991ccc1fb5");
+
+await client.DeactivateSubscriptionUserAsync(identifier);
+// EndDocSection
