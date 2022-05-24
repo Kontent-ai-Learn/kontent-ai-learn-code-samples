ce9518c3406414b9e67d1f3a44a092780ece3421
Jan Cerman <janc@kentico.com>
2022-05-24 10:39:49 +0200

    Add missing method to webhook signature helper

--- js/using-webhooks/ValidateSignature.js
+++ js/using-webhooks/ValidateSignature.js
@@ -4,7 +4,7 @@ const signatureHelper = require('@kentico/kontent-webhook-helper');
 
 // Example of generating the hash to verify the notification
 const isValidSignature = (req, secret) => {
-  return signatureHelper(
+  return signatureHelper.isValidSignatureFromString(
     req.body, // Use raw body data from the request, i.e., by using body-parser
     secret,
     req.headers['x-kc-signature']
