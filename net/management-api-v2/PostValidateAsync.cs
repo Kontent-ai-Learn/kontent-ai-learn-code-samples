b13bf845e2e9408bc9483e50425bc647298c06ca
Jiri Kotasek <10132584+gormal@users.noreply.github.com>
2022-08-21 19:48:38 +0200

    CTC-1752 Add code samples for new validation endpoints (#59)

---
+++ net/management-api-v2/PostValidateAsync.cs
@@ -0,0 +1,12 @@
+// DocSection: mapi_v2_post_validate_async
+// Tip: Find more about .NET SDKs at https://kontent.ai/learn/net
+using Kentico.Kontent.Management;
+
+var client = new ManagementClient(new ManagementOptions
+{
+    ApiKey = "<YOUR_API_KEY>",
+    ProjectId = "<YOUR_PROJECT_ID>"
+});
+
+var response = await client.InitiateProjectAsyncValidationTaskAsync();
+// EndDocSection
