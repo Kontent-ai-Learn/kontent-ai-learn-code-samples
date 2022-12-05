bad7a76d2f06d3ea2f93cf35cff56a877b91aed2
Jan Cerman <janc@kontent.ai>
2022-12-05 13:06:01 +0100

    CTC-1843 SmartLink refresh handler

---
+++ js/preview-content/SmartLink.js
@@ -0,0 +1,15 @@
+// DocSection: smartlink_refresh_handler
+// Tip: Find more about JS/TS SDKs at https://kontent.ai/learn/javascript
+import KontentSmartLink, { KontentSmartLinkEvent } from '@kontent-ai/smart-link';
+
+const sdk = KontentSmartLink.initialize({ ... });
+
+sdk.on(
+  KontentSmartLinkEvent.Refresh,
+  (data, metadata, originalRefresh) => {
+    // Custom refresh logic goes here
+    // ...
+    originalRefresh(); // If you want to refresh the page at the end
+  }
+);
+// EndDocSection
