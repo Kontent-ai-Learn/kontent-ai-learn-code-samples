6e820989d27eda8f70b65de20147a629c98c060c
Robert Stebel <roberts@kontent.ai>
2023-02-01 11:35:17 +0100

    CTC-1854 Fix sample for creating a space in .NET SDK (#68)

--- net/management-api-v2/PostSpace.cs
+++ net/management-api-v2/PostSpace.cs
@@ -8,10 +8,7 @@ var client = new ManagementClient(new ManagementOptions
     ProjectId = "<YOUR_PROJECT_ID>"
 });
 
-var identifier = Reference.ById(Guid.Parse("6291c693-f6e4-4a6b-ac67-5c31c32f9388"));
-// var identifier = Reference.ByCodename("space_1");
-
-var response = await client.ModifySpaceAsync(identifier, new SpaceCreateRequestModel
+var response = await client.CreateSpaceAsync(new SpaceCreateRequestModel
     {
         Name = "Space 1", 
         Codename = "space_1"
