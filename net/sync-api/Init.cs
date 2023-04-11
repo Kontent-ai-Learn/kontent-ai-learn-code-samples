8f04627438da82d0996207385bb60ff38346d758
František Záhora <47626763+arguit@users.noreply.github.com>
2023-04-11 15:12:42 +0200

    CTC-1961 Add sync net request samples (#72)
    * CTC-1961 Add sync net request samples
    
    * CTC-1981 Change code sample IDs and rename files
    
    ---------
    
    Co-authored-by: Frantisek Zahora <FrantisekZ@kontent.ai>
    Co-authored-by: Jan Cerman <janc@kontent.ai>

---
+++ net/sync-api/Init.cs
@@ -0,0 +1,18 @@
+﻿// DocSection: sync_api_initialize_sync
+// Tip: Find more about .NET SDKs at https://kontent.ai/learn/net
+using Kontent.Ai.Delivery;
+
+// Creates an instance of the delivery client
+// ProTip: Use DI for this in your apps https://kontent.ai/learn/net-register-client
+IDeliveryClient client = DeliveryClientBuilder
+    .WithProjectId("<YOUR_PROJECT_ID>")
+    .Build();
+
+// Gets the continuation token based on the given parameters
+IDeliverySyncInitResponse response = await client.PostSyncInitAsync(
+    new LanguageParameter("en-US"),
+    new EqualsFilter("system.type", "article"),
+    new NotEqualsFilter("system.collection", "default"));
+    
+string continuationToken = response.ApiResponse.ContinuationToken;
+// EndDocSection
