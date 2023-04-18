2c462b1dad62a5d61748721e4a98dc07e94767e3
Nicholas Margaritondo <nick.margaritondo@outlook.com>
2023-04-18 15:05:54 -0500

    added samples for 3 validation endpoints

---
+++ js/management-api-v2/GetValidationResults.js
@@ -0,0 +1,14 @@
+// DocSection: cm_api_v2_get_validation_results
+// Tip: Find more about JS/TS SDKs at https://kontent.ai/learn/javascript
+// Using ES6 syntax
+import { ManagementClient } from '@kontent-ai/management-sdk';
+
+const client = new ManagementClient({
+  projectId: '<YOUR_PROJECT_ID>',
+  apiKey: '<YOUR_API_KEY>'
+});
+
+const response = await client.listEnvironmentValidationIssues()
+.byTaskId('15c68e60-dc30-4d34-a921-396563abd08d')
+.toPromise();
+// EndDocSection
