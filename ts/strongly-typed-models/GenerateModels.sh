4120dda7f85501f8cd04ae4df0e628f867a59a3b
Jan Cerman <janc@kontent.ai>
2023-09-15 15:16:27 +0200

    Update model generate example for TS and JS

--- ts/strongly-typed-models/GenerateModels.sh
+++ ts/strongly-typed-models/GenerateModels.sh
@@ -1,5 +1,5 @@
 # DocSection: strongly_typed_models_generators
 npm i @kontent-ai/model-generator -g
 
-kontent-generate --projectId=8d20758c-d74c-4f59-ae04-ee928c0816b7 --addTimestamp=false --nameResolver=camelCase --sdkType=delivery --apiKey=<YOUR_MANAGEMENT_API_KEY>
+kontent-generate --environmentId=<YOUR_ENVIRONMENT_ID> --apiKey=<YOUR_MANAGEMENT_API_KEY>
 # EndDocSection
