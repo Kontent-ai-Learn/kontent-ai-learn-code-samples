814d37c15fa2c98da2d81051c86639365b1f23b3
Jan Cerman <janc@kontent.ai>
2023-09-15 15:20:44 +0200

    Use YOUR_ENVIRONMENT_ID in Ruby samples

--- ruby/delivery-api/GetTypes.rb
+++ ruby/delivery-api/GetTypes.rb
@@ -2,7 +2,7 @@
 # Tip: Find more about Ruby SDKs at https://kontent.ai/learn/ruby
 require 'delivery-sdk-ruby'
 
-delivery_client = Kontent::Ai::Delivery::DeliveryClient.new project_id: '<YOUR_PROJECT_ID>'
+delivery_client = Kontent::Ai::Delivery::DeliveryClient.new project_id: '<YOUR_ENVIRONMENT_ID>'
 delivery_client.types
                .limit(3)
                .execute do |response|
