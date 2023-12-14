ff3584801a0bbe1ece1af218b184bda0ca09b80f
lenkaklimcikova <49123299+lenkaklimcikova@users.noreply.github.com>
2023-12-14 15:03:17 +0100

    DEL-4734 - Add MAPI code samples for webhooks (#76)
    Co-authored-by: Lenka <LenkaK@kontent.ai>

--- net/management-api-v2/cm_api_v2_get_legacy_webhook.cs
+++ net/management-api-v2/cm_api_v2_get_legacy_webhook.cs
@@ -1,4 +1,4 @@
-// Tip: Find more about .NET SDKs at https://kontent.ai/learn/net
+﻿// Tip: Find more about .NET SDKs at https://kontent.ai/learn/net
 using Kontent.Ai.Management;
 
 var client = new ManagementClient(new ManagementOptions
@@ -9,4 +9,4 @@ var client = new ManagementClient(new ManagementOptions
 
 var identifier = Reference.ById(Guid.Parse("5df74e27-1213-484e-b9ae-bcbe90bd5990"));
 
-var response = await client.GetWebhookAsync(identifier);
+var response = await client.GetLegacyWebhookAsync(identifier);
