b734508bfc3961b6efe0abab5cd342f76f00018d
Adam Zavadil <AdamZ@kontent.ai>
2024-01-22 12:38:29 +0100

    Add get variants endpoint samples

---
+++ js/management-api-v2/cm_api_v2_get_variants_by_collection.js
@@ -0,0 +1,18 @@
+// Tip: Find more about JS/TS SDKs at https://kontent.ai/learn/javascript
+// Using ES6 syntax
+import { ManagementClient } from '@kontent-ai/management-sdk';
+
+const client = new ManagementClient({
+    environmentId: '<YOUR_ENVIRONMENT_ID>',
+    apiKey: '<YOUR_API_KEY>'
+});
+
+// Gets the first page of results
+const response = await client.listLanguageVariantsByCollection()
+    .byCollectionCodename('important_collection')
+    .toPromise();
+
+// Gets all pages of results
+// response = await client.listLanguageVariantsByCollection()
+//     .byCollectionCodename('important_collection')
+//     .toPromise();
