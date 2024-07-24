ca25ec0d2283a54abbab7112680df1ba3ec84ffe
František Záhora <47626763+arguit@users.noreply.github.com>
2024-07-24 13:38:08 +0200

    CTC-2504 Add WebSpotlight samples (#86)
    Co-authored-by: František Záhora <FrantisekZ@kontent.ai>

---
+++ net/management-api-v2/PutWebSpotlightActivate.cs
@@ -0,0 +1,20 @@
+// DocSection: cm_api_v2_put_web_spotlight_activate
+// Tip: Find more about .NET SDKs at https://kontent.ai/learn/net
+using Kontent.Ai.Management;
+
+var client = new ManagementClient(new ManagementOptions
+{
+    ApiKey = "<YOUR_API_KEY>",
+    ProjectId = "<YOUR_PROJECT_ID>"
+});
+
+var reference = Reference.ById(Guid.Parse("6291c693-f6e4-4a6b-ac67-5c31c32f9388"));
+// var reference = Reference.ById("my_web_root_type");
+
+var model = new WebSpotlightActivateModel()
+{
+    RootType = reference
+}
+
+var response = await client.ActivateWebSpotlightAsync(model);
+// EndDocSection
