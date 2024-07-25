a2232d173ee8818a82848033ab4cd64431a77d45
Jan Cerman <janc@kontent.ai>
2024-07-25 14:27:23 +0200

    CTC-2504 Add external ID reference

--- net/management-api-v2/mapi_v2_put_web_spotlight_activate.cs
+++ net/management-api-v2/mapi_v2_put_web_spotlight_activate.cs
@@ -9,6 +9,7 @@ var client = new ManagementClient(new ManagementOptions
 
 var reference = Reference.ByCodename("custom_web_spotlight_root");
 // var reference = Reference.ById(Guid.Parse("6291c693-f6e4-4a6b-ac67-5c31c32f9388"));
+// var reference = Reference.ByExternalId("custom-root-type");
 
 var model = new WebSpotlightActivateModel()
 {
