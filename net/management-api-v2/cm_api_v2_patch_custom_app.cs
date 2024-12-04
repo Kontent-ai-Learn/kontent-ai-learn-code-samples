04ed6dae48daba66079f45c9e881be37d9189ba3
Jan Cerman <janc@kontent.ai>
2024-12-04 13:12:06 +0100

    CTC-2631 Fix a typo

--- net/management-api-v2/cm_api_v2_patch_custom_app.cs
+++ net/management-api-v2/cm_api_v2_patch_custom_app.cs
@@ -7,7 +7,7 @@ var client = new ManagementClient(new ManagementOptions
     EnvironmentId = "KONTENT_AI_ENVIRONMENT_ID"
 });
 
-var identifier = Reference.ById(Guid.Parse("f4b3fc05-e988-4dae-9ac1-a94aba566474");
+var identifier = Reference.ById(Guid.Parse("f4b3fc05-e988-4dae-9ac1-a94aba566474"));
 //var identifier = Reference.ByCodename("my_custom_app");
 
 var changes = new CustomAppOperationBaseModel[]
