92da0ffa64fd7b7f5126aab2f07af98026abe517
Jan Cerman <janc@kontent.ai>
2024-12-04 08:35:21 +0100

    CTC-2631 Correct modify custom apps in .NET

--- net/management-api-v2/cm_api_v2_patch_custom_app.cs
+++ net/management-api-v2/cm_api_v2_patch_custom_app.cs
@@ -7,6 +7,9 @@ var client = new ManagementClient(new ManagementOptions
     EnvironmentId = "KONTENT_AI_ENVIRONMENT_ID"
 });
 
+var identifier = Reference.ById(Guid.Parse("f4b3fc05-e988-4dae-9ac1-a94aba566474");
+//var identifier = Reference.ByCodename("my_custom_app");
+
 var changes = new CustomAppOperationBaseModel[]
 {
     new CustomAppAddIntoPatchModel()
@@ -56,4 +59,4 @@ var changes = new CustomAppOperationBaseModel[]
     }
 };
 
-var response = await client.ModifyCustomAppAsync(customAppCreateModel);
+var response = await client.ModifyCustomAppAsync(identifier, customAppCreateModel);
