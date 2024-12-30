e5e92e709465ecca270cedbb9ef6458ca990550a
Jan Cerman <janc@kontent.ai>
2024-12-30 14:01:41 +0100

    Remove examples for filtering by supages

--- ruby/filter-content/filtering_get_subpages.rb
+++
@@ -1,5 +0,0 @@
-# Gets pages linking travel insurance as their subpage.
-delivery_client.items('elements.subpages'.contains('travel_insurance'))
-
-# Gets pages linking at least travel insurance, car insurance, or both as their subpage.
-delivery_client.items('elements.subpages'.any('travel_insurance', 'car_insurance'))
