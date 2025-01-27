0b12adef7c26ba3994be5804ccb2ca7835fb5c96
Jan Cerman <janc@kontent.ai>
2025-01-27 19:00:33 +0100

    Add elementResolver example

--- ts/strongly-typed-models/strongly_typed_models_generators.sh
+++ ts/strongly-typed-models/strongly_typed_models_generators.sh
@@ -1,3 +1,3 @@
 npm i @kontent-ai/model-generator -g
 
-kontent-generate --environmentId=KONTENT_AI_ENVIRONMENT_ID --apiKey=KONTENT_AI_MANAGEMENT_API_KEY
+kontent-generate --environmentId=KONTENT_AI_ENVIRONMENT_ID --apiKey=KONTENT_AI_MANAGEMENT_API_KEY --elementResolver=camelCase
