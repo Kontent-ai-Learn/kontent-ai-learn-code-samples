c7f2ab96ecc7bd3a3cd0b1b1d88112e3bd86a23a
Daniel Pokorny <52500882+pokornyd@users.noreply.github.com>
2025-01-28 12:49:10 +0100

    update TS rich text resolution (#96)
    * update TS rich text resolution
    
    * rename item link and componentOrItem props
    
    * CTC-2701 Adjust formatting
    
    ---------
    
    Co-authored-by: Jan Cerman <janc@kontent.ai>

--- ts/resolve-rich-text/resolve_rich_text_components_items.tsx
+++ ts/resolve-rich-text/resolve_rich_text_components_items.tsx
@@ -1,25 +1,22 @@
 import { Elements } from "@kontent-ai/delivery-sdk";
-import { PortableTextComponent } from "@kontent-ai/rich-text-resolver";
-import { PortableTextComponents, PortableTextTypeComponentProps, } from "@portabletext/react";
-import { React } from 'react';
+import { PortableTextReactResolvers } from "@kontent-ai/rich-text-resolver/utils/react";
+import { React } from "react";
 
 import { CalloutComponent } from "../components/shared/richText/Callout";
 import { contentTypes } from "../models";
 
-const createRichTextResolver = (element: Elements.RichTextElement): Partial<PortableTextComponents> => ({
+const createRichTextResolver = (element: Elements.RichTextElement): PortableTextReactResolvers => ({
   types: {
     // Components and content items inserted in rich text
-    component: ({
-      value,
-    }: PortableTextTypeComponentProps<PortableTextComponent>) => {
+    component: ({ value }) => {
       // Checks if the inserted content item is included in the response
       // Kontent.ai components are always included in the response
       const componentOrItem = element.linkedItems.find(
-        (i) => i.system.codename === value.component._ref
+        (i) => i.system.codename === value.componentOrItem._ref
       );
 
       if (!componentOrItem) {
-        return (<div>The inserted content item wasn't found.</div>);
+        return <div>The inserted content item wasn't found.</div>;
       }
 
       // Renders the component or content item based on its type
@@ -28,7 +25,11 @@ const createRichTextResolver = (element: Elements.RichTextElement): Partial<Port
         case contentTypes.callout.codename:
           return <CalloutComponent item={componentOrItem} />;
         default:
-          return (<div>Content based on the type '{componentOrItem.system.type}' couldn't be resolved.</div>);
+          return (
+            <div>
+              Content based on the type '{componentOrItem.system.type}' couldn't be resolved.
+            </div>
+          );
       }
     },
   },
