5e50669db9f84a908f108075f014183fec9fd18b
Martin Ševčík <martins2@kontent.ai>
2025-03-20 15:16:18 +0100

    introduce code samples for used in endpoints

---
+++ net/delivery-api/delivery_api_get_asset_used_in.cs
@@ -0,0 +1,21 @@
+// Tip: Find more about .NET SDKs at https://kontent.ai/learn/net
+using Kontent.Ai.Delivery;
+
+// Creates an instance of the delivery client; see https://kontent.ai/learn/net-register-client
+IDeliveryClient client = DeliveryClientBuilder
+      .WithEnvironmentId("KONTENT_AI_ENVIRONMENT_ID")
+      .Build();
+
+// Gets feed of all strongly typed parent content items of type article for asset 'my_asset'
+IDeliveryItemsFeed<IUsedInItem> feed = client.GetAssetUsedIn("my_asset",
+    new EqualsFilter("system.type", "article")
+    );
+
+while (feed.HasMoreResults)
+{
+    IDeliveryItemsFeedResponse<IUsedInItem> response = await feed.FetchNextBatchAsync();
+    foreach(IUsedInItem usedInItem in response) {
+        // Do something with the parent content item, e.g. update cache
+        ProcessUsedInItem(usedInItem);
+    }
+}
