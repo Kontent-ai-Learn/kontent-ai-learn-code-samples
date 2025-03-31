85c236aabf46f18dd4e0ff24e7b007f845210422
Richard Sustek <ennspassion@gmail.com>
2025-03-31 13:33:43 +0200

    Updates strongly typed models (ts)

--- ts/strongly-typed-models/strongly_typed_models_generators.sh
+++ ts/strongly-typed-models/strongly_typed_models_generators.sh
@@ -1,3 +1,1 @@
-npm i @kontent-ai/model-generator -g
-
-kontent-generate --environmentId=KONTENT_AI_ENVIRONMENT_ID --apiKey=KONTENT_AI_MANAGEMENT_API_KEY --elementResolver=camelCase
+npx @kontent-ai/model-generator@latest delivery-sdk --environmentId=x --managementApiKey=x
