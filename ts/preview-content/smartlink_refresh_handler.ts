1b5ef0328eca84c60ac7ff158dfe983dd0df603d
AndreeaTudor7 <andreea.tudor@kontent.ai>
2025-07-24 15:41:43 +0200

    CTC-2827 - Update smartlink_refresh_handler.ts
    There was an empty comment, so I added some context.

--- ts/preview-content/smartlink_refresh_handler.ts
+++ ts/preview-content/smartlink_refresh_handler.ts
@@ -1,7 +1,7 @@
 // Tip: Find more about JS/TS SDKs at https://kontent.ai/learn/javascript
 import KontentSmartLink, { KontentSmartLinkEvent } from '@kontent-ai/smart-link';
 
-// 
+// Initializes the Smart Link SDK using your environment ID and language codename
 const sdk = KontentSmartLink.initialize({
   defaultDataAttributes: {
     environmentId: 'KONTENT_AI_ENVIRONMENT_ID',
