0b7a096017cef3be0bb3ff96ffb9b653bf0d60fe
František Záhora <FrantisekZ@kontent.ai>
2025-09-11 10:59:50 +0200

    EN-542 Add .NET code samples for filtering variants endpoint

---
+++ net/management-api-v2/cm_api_v2_post_filter_variants.cs
@@ -0,0 +1,45 @@
+// Tip: Find more about .NET SDKs at https://kontent.ai/learn/net
+using Kontent.Ai.Management;
+
+var client = new ManagementClient(new ManagementOptions
+{
+    ApiKey = "KONTENT_AI_MANAGEMENT_API_KEY",
+    EnvironmentId = "KONTENT_AI_ENVIRONMENT_ID"
+});
+
+// Some SDK queries are available through the `EarlyAccess` client.
+// These APIs are experimental and may change without a major version bump.
+// Use with caution in production environments.
+
+// Minimal request
+var response = await client.EarlyAccess.FilterVariantsAsync(new VariantFilterRequestModel
+{
+    Filters = new VariantFilterFiltersModel
+    {
+        Language = Reference.ByCodename("en-US")
+    }
+});
+
+// Rich request
+var response = await client.EarlyAccess.FilterVariantsAsync(new VariantFilterRequestModel
+{
+    Filters = new VariantFilterFiltersModel
+    {
+        SearchPhrase = "test",
+        Language = Reference.ByCodename("en-US"),
+        ContentTypes = new List<Reference>
+        {
+            Reference.ByCodename("article")
+        },
+        CompletionStatuses = new List<VariantFilterCompletionStatus> 
+        { 
+            VariantFilterCompletionStatus.Completed 
+        }
+    },
+    Order = new VariantFilterOrderModel
+    {
+        By = "name",
+        Direction = VariantFilterOrderDirection.Ascending
+    },
+    IncludeContent = true
+});
