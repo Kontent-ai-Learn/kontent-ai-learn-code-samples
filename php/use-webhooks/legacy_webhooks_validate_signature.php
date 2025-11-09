66bc8dffc66085dce9e6b892bab9c7742ac30814
Martin Ševčík <martins2@kontent.ai>
2025-11-09 11:18:49 +0100

    EN-628 remove legacy webhooks code samples

--- php/use-webhooks/legacy_webhooks_validate_signature.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Example of generating the hash to verify the notification
-$givenSignature = $_SERVER['HTTP_X_KC_SIGNATURE'];
-$computedSignature = base64_encode(hash_hmac('sha256', $json_message, $secret, true));
-
-$result = hash_equals($givenSignature, $computedSignature);
-?>
